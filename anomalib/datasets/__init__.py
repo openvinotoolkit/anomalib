6e9ca39afcc05aebc4ab7aace2461cbee3d4daaa
Samet Akcay <samet.akcay@cosmonio.com>
2021-05-03 16:51:10 +0100

    type checking in data module getter

--- anomalib/datasets/__init__.py
+++ anomalib/datasets/__init__.py
@@ -1,12 +1,12 @@
 from argparse import Namespace
 from typing import Dict, Union
-
+from omegaconf import DictConfig, ListConfig
 from pytorch_lightning import LightningDataModule
 
 from .mvtec import MVTecDataModule
 
 
-def get_datamodule(args: Union[Dict, Namespace]):
+def get_datamodule(args: Union[DictConfig, ListConfig]):
     datamodule: LightningDataModule
     if args.dataset.lower() == "mvtec":
         datamodule = MVTecDataModule(args.dataset_path, args.batch_size, args.num_workers)
