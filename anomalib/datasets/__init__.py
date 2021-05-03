0c8cbfbb0f2a25903a1386e9fb5566604473f44b
Samet Akcay <samet.akcay@cosmonio.com>
2021-05-03 11:34:30 +0100

    add datamodule getter to import datasets

--- anomalib/datasets/__init__.py
+++ anomalib/datasets/__init__.py
@@ -1,1 +1,16 @@
-from .mvtec import MVTecDataModule, get_image_transforms
+from argparse import Namespace
+from typing import Dict, Union
+
+from pytorch_lightning import LightningDataModule
+
+from .mvtec import MVTecDataModule
+
+
+def get_datamodule(args: Union[Dict, Namespace]):
+    datamodule: LightningDataModule
+    if args.dataset.lower() == "mvtec":
+        datamodule = MVTecDataModule(args.dataset_path, args.batch_size, args.num_workers)
+    else:
+        raise ValueError("Unknown dataset!")
+
+    return datamodule
