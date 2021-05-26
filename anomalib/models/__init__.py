6af1649cd6d3a429eacc2a79c5c3ef52508b74f4
Samet Akcay <samet.akcay@cosmonio.com>
2021-05-26 18:35:41 +0100

    get_model reads the main config now

--- anomalib/models/__init__.py
+++ anomalib/models/__init__.py
@@ -3,13 +3,13 @@ from .stfpm import STFPMModel
 from .dfkde import DFKDEModel
 
 
-def get_model(args):
-    if args.model == "padim":
-        model = PADIMModel(args)
-    elif args.model == "stfpm":
-        model = STFPMModel(args)
-    elif args.model == "dfkde":
-        model = DFKDEModel(args)
+def get_model(config):
+    if config.model.name == "padim":
+        model = PADIMModel(config)
+    elif config.model.name == "stfpm":
+        model = STFPMModel(config)
+    elif config.model.name == "dfkde":
+        model = DFKDEModel(config)
     else:
         raise ValueError("Unknown model name!")
 
