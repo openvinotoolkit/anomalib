c5460814e9edcd7d9bd00db69e4793688579843c
Samet Akcay <samet.akcay@cosmonio.com>
2021-05-26 18:38:56 +0100

    cleanup train.py

--- train.py
+++ train.py
@@ -11,15 +11,14 @@ def get_args():
     parser = ArgumentParser()
     parser.add_argument("--model", type=str, default="padim", help="Name of the algorithm to train/test")
     parser.add_argument("--model_config_path", type=str, required=False, help="Path to a model config file")
-    args = parser.parse_args()
-    return args
+
+    return parser.parse_args()
 
 
 args = get_args()
-
 config = get_configurable_parameters(model_name=args.model, model_config_path=args.model_config_path)
-datamodule = get_datamodule(config.dataset)
-model = get_model(config.model)
+datamodule = get_datamodule(config)
+model = get_model(config)
 
 trainer = Trainer(callbacks=model.callbacks, **config.trainer)
 trainer.fit(model=model, datamodule=datamodule)
