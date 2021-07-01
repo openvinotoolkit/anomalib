cf32c70119f20fea88d7fe9d81a9895ba7786aad
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-01 10:09:59 +0100

    added `make_mvtec_dataset` function

--- anomalib/datasets/mvtec.py
+++ anomalib/datasets/mvtec.py
@@ -7,24 +7,138 @@ This script contains PyTorch Dataset, Dataloader and PyTorch Lightning
 """
 
 import logging
+import random
 import tarfile
 from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from urllib.request import urlretrieve
 
+import pandas as pd
 import torch
 import torchvision.transforms as T
 from PIL import Image
+from pandas.core.frame import DataFrame
 from pytorch_lightning.core.datamodule import LightningDataModule
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
-from torchvision.datasets.folder import VisionDataset, default_loader
+from torchvision.datasets.folder import VisionDataset
 from torchvision.transforms import Compose
 
 logger = logging.getLogger(name="Dataset: MVTec")
 
 __all__ = ["MVTec", "MVTecDataModule"]
+
+
+def split_normal_images_in_train_set(samples: DataFrame, split_ratio: float = 0.1, seed: int = 0) -> DataFrame:
+    """
+    split_normal_images_in_train_set
+        This function splits the normal images in training set and assigns the
+        values to the test set. This is particularly useful especially when the
+        test set does not contain any normal images.
+
+        This is important because when the test set doesn't have any normal images,
+        AUC computation fails due to having single class.
+
+    Args:
+        samples (DataFrame): Dataframe containing dataset info such as filenames, splits etc.
+        split_ratio (float, optional): Train-Test normal image split ratio. Defaults to 0.1.
+        seed (int, optional): Random seed to ensure reproducability. Defaults to 0.
+
+    Returns:
+        DataFrame: Output dataframe where the part of the training set is assigned to test set.
+    """
+
+    if seed > 0:
+        random.seed(seed)
+
+    normal_train_image_indices = samples.index[(samples.split == "train") & (samples.label == "good")].to_list()
+    num_normal_train_images = len(normal_train_image_indices)
+    num_normal_valid_images = int(num_normal_train_images * split_ratio)
+
+    indices_to_split_from_train_set = random.sample(population=normal_train_image_indices, k=num_normal_valid_images)
+    samples.loc[indices_to_split_from_train_set, "split"] = "test"
+
+    return samples
+
+
+def make_mvtec_dataset(path: Path, split: str = "train", split_ratio: float = 0.1, seed: int = 0) -> DataFrame:
+    """
+    This function creates MVTec samples by parsing the MVTec data file structure, based on the following
+    structure:
+        path/to/dataset/split/category/image_filename.png
+        path/to/dataset/ground_truth/mask_filename.png
+
+    This function creates a dataframe to store the parsed information based on the following format:
+    |---|---------------|-------|---------|---------------|---------------------------------------|-------------|
+    |   | path          | split | label   | image_path    | mask_path                             | label_index |
+    |---|---------------|-------|---------|---------------|---------------------------------------|-------------|
+    | 0 | datasets/name |  test |  defect |  filename.png | ground_truth/defect/filename_mask.png | 1           |
+    |---|---------------|-------|---------|---------------|---------------------------------------|-------------|
+
+    Args:
+        path (Path): Path to dataset
+        split (str, optional): Dataset split (ie., either train or test). Defaults to "train".
+        split_ratio (float, optional): Ratio to split normal training images and add to the
+                                       test set in case test set doesn't contain any normal images.
+                                       Defaults to 0.1.
+        seed (int, optional): Random seed to ensure reproducibility when splitting. Defaults to 0.
+
+    Example:
+        The following example shows how to get training samples from MVTec bottle category:
+
+        >>> root = Path('./MVTec')
+        >>> category = 'bottle'
+        >>> path = root / category
+        >>> path
+        PosixPath('MVTec/bottle')
+
+        >>> samples = make_mvtec_dataset(path, split='train', split_ratio=0.1, seed=0)
+        >>> samples.head()
+           path         split label image_path                           mask_path                   label_index
+        0  MVTec/bottle train good MVTec/bottle/train/good/105.png MVTec/bottle/ground_truth/good/105_mask.png 0
+        1  MVTec/bottle train good MVTec/bottle/train/good/017.png MVTec/bottle/ground_truth/good/017_mask.png 0
+        2  MVTec/bottle train good MVTec/bottle/train/good/137.png MVTec/bottle/ground_truth/good/137_mask.png 0
+        3  MVTec/bottle train good MVTec/bottle/train/good/152.png MVTec/bottle/ground_truth/good/152_mask.png 0
+        4  MVTec/bottle train good MVTec/bottle/train/good/109.png MVTec/bottle/ground_truth/good/109_mask.png 0
+
+    Returns:
+        DataFrame: an output dataframe containing samples for the requested split (ie., train or test)
+    """
+    samples_list = [(str(path),) + filename.parts[3:] for filename in path.glob("**/*.png")]
+    if len(samples_list) == 0:
+        raise RuntimeError(f"Found 0 images in {path}")
+
+    samples = pd.DataFrame(samples_list, columns=["path", "split", "label", "image_path"])
+    samples = samples[samples.split != "ground_truth"]
+
+    # Create mask_path column
+    samples["mask_path"] = (
+        samples.path + "/ground_truth/" + samples.label + "/" + samples.image_path.str.rstrip(".png") + "_mask.png"
+    )
+
+    # Modify image_path column by converting to absolute path
+    samples["image_path"] = samples.path + "/" + samples.split + "/" + samples.label + "/" + samples.image_path
+
+    # Split the normal images in training set if test set doesn't
+    # contain any normal images. This is needed because AUC score
+    # cannot be computed based on 1-class
+    if sum((samples.split == "test") & (samples.label == "good")) == 0:
+        samples = split_normal_images_in_train_set(samples, split_ratio, seed)
+
+    # Good images don't have mask
+    samples.loc[(samples.split == "test") & (samples.label == "good"), "mask_path"] = ""
+
+    # Create label index for train (0) and test (1) sets.
+    samples.loc[samples.split == "train", "label_index"] = 0
+    samples.loc[samples.split == "test", "label_index"] = 1
+    samples.label_index = samples.label_index.astype(int)
+
+    # Get the data frame for the split.
+    samples = samples[samples.split == split]
+    samples = samples.reset_index(drop=True)
+
+    return samples
 
 
 def get_image_transforms() -> T.Compose:
@@ -82,18 +196,22 @@ class MVTec(VisionDataset):
     ) -> None:
         super().__init__(root, transform=image_transforms, target_transform=mask_transforms)
         self.root = Path(root) if isinstance(root, str) else root
-        self.category: Path = self.root / category
+        self.category: str = category
+        # TODO: Commented out the below line.
+        # self.category: Path = self.root / category
         self.split = "train" if train else "test"
         self.image_transforms = image_transforms if image_transforms is not None else get_image_transforms()
         self.mask_transforms = mask_transforms if mask_transforms is not None else get_mask_transforms()
-        self.loader = loader if loader is not None else default_loader
+        # self.loader = loader if loader is not None else default_loader
         self.download = download
-        self.exclude_normal_images_in_validation = exclude_normal_images_in_validation
+        # self.exclude_normal_images_in_validation = exclude_normal_images_in_validation
 
         if self.download:
             self._download()
 
-        self.samples = self.make_dataset()
+        # TODO: Changed this
+        # self.samples = self.make_dataset()
+        self.samples = make_mvtec_dataset(path=self.root / category, split=self.split)
         if len(self.samples) == 0:
             raise RuntimeError(f"Found 0 images in {self.category / self.split}")
 
@@ -101,7 +219,9 @@ class MVTec(VisionDataset):
         """
         Download the MVTec dataset
         """
-        if self.category.is_dir():
+        if Path(self.category).is_dir():
+        # TODO: Changed this.
+        # if self.category.is_dir():
             logger.warning("Dataset directory exists.")
         else:
             self.root.mkdir(parents=True, exist_ok=True)
@@ -164,7 +284,11 @@ class MVTec(VisionDataset):
         return len(self.samples)
 
     def __getitem__(self, index: int) -> Union[Dict[str, Tensor], Dict[str, Union[str, Tensor]]]:
-        image_path, mask_path, label_index = self.samples[index]
+        # TODO: Changed this.
+        # image_path, mask_path, label_index = self.samples[index]
+        image_path = self.samples.image_path[index]
+        mask_path = self.samples.mask_path[index]
+        label_index = self.samples.label_index[index]
 
         image = Image.open(image_path).convert("RGB")
         image_tensor = self.image_transforms(image)
@@ -214,7 +338,7 @@ class MVTecDataModule(LightningDataModule):
         self.num_workers = num_workers
         self.image_transforms = image_transforms
         self.mask_transforms = mask_transforms
-        self.loader = default_loader if loader is None else loader
+        # self.loader = default_loader if loader is None else loader
         self.exclude_normal_images_in_validation = exclude_normal_images_in_validation
 
         self.train_data: Dataset
