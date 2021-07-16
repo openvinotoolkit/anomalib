62bfbad2e5e9b6f7f4eb7fed08f7072b8f8da017
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-16 16:42:00 +0100

    Improved coverage

--- tests/datasets/test_tiler.py
+++ tests/datasets/test_tiler.py
@@ -6,7 +6,7 @@ import pytest
 import torch
 from omegaconf import ListConfig
 
-from anomalib.datasets.utils import StrideSizeError, Tiler
+from anomalib.datasets.tiler import StrideSizeError, Tiler
 
 tile_data = [
     ([3, 1024, 1024], 512, 512, torch.Size([4, 3, 512, 512])),
@@ -55,6 +55,16 @@ def test_stride_size_cannot_be_larger_than_tile_size():
         tiler = Tiler(tile_size=kernel_size, stride=stride)
 
 
+def test_tile_size_cannot_be_larger_than_image_size():
+    """
+    Larger tile size than image size is not desired, and causes issues.
+    """
+    with pytest.raises(ValueError):
+        tiler = Tiler(tile_size=1024, stride=512)
+        image = torch.rand(1, 3, 512, 512)
+        tiler.tile(image)
+
+
 @pytest.mark.parametrize("tile_size, kernel_size, stride, image_size", untile_data)
 def test_untile_non_overlapping_patches(tile_size, kernel_size, stride, image_size):
     """
@@ -66,6 +76,12 @@ def test_untile_non_overlapping_patches(tile_size, kernel_size, stride, image_si
 
     untiled_image = tiler.untile(tiles)
     assert untiled_image.shape == torch.Size(image_size)
+
+
+@pytest.mark.parametrize("mode", ["pad", "padded", "interpolate", "interplation"])
+def test_upscale_downscale_mode(mode):
+    with pytest.raises(ValueError):
+        tiler = Tiler(tile_size=(512, 512), stride=(256, 256), mode=mode)
 
 
 @pytest.mark.parametrize("image_size, kernel_size, stride, tile_size, mode", overlapping_data)
@@ -88,7 +104,7 @@ def test_untile_overlapping_patches(image_size, kernel_size, stride, tile_size, 
 def test_divisible_tile_size_and_stride(image_size, tile_size, stride, mode):
     """
     When the image is not divisible by tile size and stride,
-        Tiler should upsamples the image before tiling, and downscales
+        Tiler should up samples the image before tiling, and downscales
         before untiling.
     """
     tiler = Tiler(tile_size, stride, mode)
