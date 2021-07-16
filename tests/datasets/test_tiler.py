a3f67f39ae5cdfeb8aea52a37b837d3b4b8ac825
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-16 15:53:51 +0100

    Updated tests

--- tests/datasets/test_tiler.py
+++ tests/datasets/test_tiler.py
@@ -1,3 +1,7 @@
+"""
+Image Tiling Tests.
+"""
+
 import pytest
 import torch
 from omegaconf import ListConfig
@@ -22,6 +26,9 @@ overlapping_data = [
 
 @pytest.mark.parametrize("tile_size, stride", [(512, 256), ([512, 512], [256, 256]), (ListConfig([512, 512]), 256)])
 def test_size_types_should_be_int_tuple_or_list_config(tile_size, stride):
+    """
+    Size type could only be integer, tuple or ListConfig type.
+    """
     tiler = Tiler(tile_size=tile_size, stride=stride)
     assert isinstance(tiler.tile_size_h, int)
     assert isinstance(tiler.stride_w, int)
@@ -29,6 +36,9 @@ def test_size_types_should_be_int_tuple_or_list_config(tile_size, stride):
 
 @pytest.mark.parametrize("image_size, tile_size, stride, shape", tile_data)
 def test_tiler_handles_single_image_without_batch_dimension(image_size, tile_size, stride, shape):
+    """
+    Tiler should add batch dimension if image is 3D (CxHxW).
+    """
     tiler = Tiler(tile_size=tile_size, stride=stride)
     image = torch.rand(image_size)
     patches = tiler.tile(image)
@@ -36,6 +46,9 @@ def test_tiler_handles_single_image_without_batch_dimension(image_size, tile_siz
 
 
 def test_stride_size_cannot_be_larger_than_tile_size():
+    """
+    Larger stride size than tile size is not desired, and causes issues.
+    """
     kernel_size = (128, 128)
     stride = 256
     with pytest.raises(StrideSizeError):
@@ -44,6 +57,9 @@ def test_stride_size_cannot_be_larger_than_tile_size():
 
 @pytest.mark.parametrize("tile_size, kernel_size, stride, image_size", untile_data)
 def test_untile_non_overlapping_patches(tile_size, kernel_size, stride, image_size):
+    """
+    Non-Overlapping Tiling/Untiling should return the same image size.
+    """
     tiler = Tiler(tile_size=kernel_size, stride=stride)
     image = torch.rand(image_size)
     tiles = tiler.tile(image)
@@ -54,6 +70,9 @@ def test_untile_non_overlapping_patches(tile_size, kernel_size, stride, image_si
 
 @pytest.mark.parametrize("image_size, kernel_size, stride, tile_size, mode", overlapping_data)
 def test_untile_overlapping_patches(image_size, kernel_size, stride, tile_size, mode):
+    """
+    Overlapping Tiling/Untiling should return the same image size.
+    """
     tiler = Tiler(tile_size=kernel_size, stride=stride, mode=mode)
 
     image = torch.rand(image_size)
@@ -62,18 +81,21 @@ def test_untile_overlapping_patches(image_size, kernel_size, stride, tile_size, 
     assert torch.equal(image, reconstructed_image)
 
 
-@pytest.mark.parametrize(
-    "image_size, tile_size, stride, mode",
-    [
-        ([1, 3, 1024, 1024], [211, 210], [197, 197], "padding"),
-        ([2, 3, 512, 512], [120, 124], [100, 100], "interpolation"),
-    ],
-)
-def test_non_divisible_tile_size_and_stride_should_be_handled(image_size, tile_size, stride, mode):
-
-    tiler = Tiler(tile_size=tile_size, stride=stride, mode=mode)
-
+@pytest.mark.parametrize("image_size", [(1, 3, 512, 512)])
+@pytest.mark.parametrize("tile_size", [(256, 256), (200, 200), (211, 213), (312, 333), (511, 511)])
+@pytest.mark.parametrize("stride", [(64, 64), (111, 111), (128, 111), (128, 128)])
+@pytest.mark.parametrize("mode", ["padding", "interpolation"])
+def test_divisible_tile_size_and_stride(image_size, tile_size, stride, mode):
+    """
+    When the image is not divisible by tile size and stride,
+        Tiler should upsamples the image before tiling, and downscales
+        before untiling.
+    """
+    tiler = Tiler(tile_size, stride, mode)
     image = torch.rand(image_size)
     tiles = tiler.tile(image)
     reconstructed_image = tiler.untile(tiles)
     assert image.shape == reconstructed_image.shape
+
+    if mode == "padding":
+        assert torch.allclose(image, reconstructed_image)
