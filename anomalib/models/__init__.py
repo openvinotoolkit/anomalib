7420c4274347221e60061aae851fedb4d0b77647
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-21 15:06:29 +0100

    registered patchcore

--- anomalib/models/__init__.py
+++ anomalib/models/__init__.py
@@ -8,6 +8,7 @@ from omegaconf import DictConfig, ListConfig
 from .dfkde.model import DFKDELightning
 from .padim.model import PADIMLightning
 from .stfpm.model import STFPMLightning, STFPMOpenVino
+from .patchcore.model import PatchcoreLightning
 
 
 def get_model(config: Union[DictConfig, ListConfig]):
@@ -35,6 +36,8 @@ def get_model(config: Union[DictConfig, ListConfig]):
             model = STFPMLightning
         elif config.model.name == "dfkde":
             model = DFKDELightning
+        elif config.model.name == 'patchcore':
+            model = PatchcoreLightning
         else:
             raise ValueError("Unknown model name!")
 
