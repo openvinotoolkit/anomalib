4b843ad6108f5d7ca804f5fabb3832fc5427717f
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-22 17:37:47 +0100

    Polish train.py

--- train.py
+++ train.py
@@ -28,33 +28,9 @@ def get_args() -> Namespace:
     return parser.parse_args()
 
 
-# def get_config():
-#     parser = ArgumentParser(description="ANOMALYDETECTION")
-#     parser.add_argument("--phase", choices=["train", "test"], default="train")
-#     parser.add_argument(
-#         "--dataset_path", default=r"/home/sakcay/Projects/data/MVTec"
-#     )  # 'D:\Dataset\mvtec_anomaly_detection')#
-#     parser.add_argument("--category", default="carpet")
-#     parser.add_argument("--num_epochs", default=1)
-#     parser.add_argument("--batch_size", default=32)
-#     parser.add_argument("--load_size", default=256)  # 256
-#     parser.add_argument("--input_size", default=224)
-#     parser.add_argument("--coreset_sampling_ratio", default=0.001)
-#     parser.add_argument(
-#         "--project_root_path", default=r"/home/sakcay/Projects/ote/anomalib/results/patchcore/mvtec/leather"
-#     )  # 'D:\Project_Train_Results\mvtec_anomaly_detection\210624\test') #
-#     parser.add_argument("--save_src_code", default=True)
-#     parser.add_argument("--save_anomaly_map", default=True)
-#     parser.add_argument("--n_neighbors", type=int, default=9)
-#     args = parser.parse_args()
-#     return args
-
-
 if __name__ == "__main__":
     args = get_args()
     config = get_configurable_parameters(model_name=args.model, model_config_path=args.model_config_path)
-
-    # c = get_config()
 
     if config.project.seed != 0:
         seed_everything(config.project.seed)
