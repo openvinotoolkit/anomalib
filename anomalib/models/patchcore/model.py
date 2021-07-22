6df2aa7890930d1a7afaf0c19f8a9d7b9cce1065
Samet Akcay <samet.akcay@cosmonio.com>
2021-07-22 07:42:31 +0100

    Use concat_layer_embeddings. Works!

--- anomalib/models/patchcore/model.py
+++ anomalib/models/patchcore/model.py
@@ -137,71 +137,6 @@ mean_train = [0.485, 0.456, 0.406]
 std_train = [0.229, 0.224, 0.225]
 
 
-# class MVTecDataset(Dataset):
-#     def __init__(self, root, transform, gt_transform, phase):
-#         if phase == "train":
-#             self.img_path = os.path.join(root, "train")
-#         else:
-#             self.img_path = os.path.join(root, "test")
-#             self.gt_path = os.path.join(root, "ground_truth")
-#         self.transform = transform
-#         self.gt_transform = gt_transform
-#         # load dataset
-#         (
-#             self.img_paths,
-#             self.gt_paths,
-#             self.labels,
-#             self.types,
-#         ) = self.load_dataset()  # self.labels => good : 0, anomaly : 1
-
-#     def load_dataset(self):
-
-#         img_tot_paths = []
-#         gt_tot_paths = []
-#         tot_labels = []
-#         tot_types = []
-
-#         defect_types = os.listdir(self.img_path)
-
-#         for defect_type in defect_types:
-#             if defect_type == "good":
-#                 img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
-#                 img_tot_paths.extend(img_paths)
-#                 gt_tot_paths.extend([0] * len(img_paths))
-#                 tot_labels.extend([0] * len(img_paths))
-#                 tot_types.extend(["good"] * len(img_paths))
-#             else:
-#                 img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
-#                 gt_paths = glob.glob(os.path.join(self.gt_path, defect_type) + "/*.png")
-#                 img_paths.sort()
-#                 gt_paths.sort()
-#                 img_tot_paths.extend(img_paths)
-#                 gt_tot_paths.extend(gt_paths)
-#                 tot_labels.extend([1] * len(img_paths))
-#                 tot_types.extend([defect_type] * len(img_paths))
-
-#         assert len(img_tot_paths) == len(gt_tot_paths), "Something wrong with test and ground truth pair!"
-
-#         return img_tot_paths, gt_tot_paths, tot_labels, tot_types
-
-#     def __len__(self):
-#         return len(self.img_paths)
-
-#     def __getitem__(self, idx):
-#         img_path, gt, label, img_type = self.img_paths[idx], self.gt_paths[idx], self.labels[idx], self.types[idx]
-#         img = Image.open(img_path).convert("RGB")
-#         img = self.transform(img)
-#         if gt == 0:
-#             gt = torch.zeros([1, img.size()[-2], img.size()[-2]])
-#         else:
-#             gt = Image.open(gt)
-#             gt = self.gt_transform(gt)
-
-#         assert img.size()[1:] == gt.size()[1:], "image.size != gt.size !!!"
-
-#         return img, gt, label, os.path.basename(img_path[:-4]), img_type
-
-
 def cvt2heatmap(gray):
     heatmap = cv2.applyColorMap(np.uint8(gray), cv2.COLORMAP_JET)
     return heatmap
@@ -342,26 +277,9 @@ class PatchcoreLightning(pl.LightningModule):
         self.model.layer2[-1].register_forward_hook(hook_t)
         self.model.layer3[-1].register_forward_hook(hook_t)
 
-        # self.criterion = torch.nn.MSELoss(reduction="sum")
-
         self.init_results_list()
 
         self.automatic_optimization = False
-        # self.data_transforms = transforms.Compose(
-        #     [
-        #         transforms.Resize(tuple(hparams.dataset.image_size), Image.ANTIALIAS),
-        #         transforms.ToTensor(),
-        #         transforms.CenterCrop(tuple(hparams.dataset.crop_size)),
-        #         transforms.Normalize(mean=mean_train, std=std_train),
-        #     ]
-        # )
-        # self.gt_transforms = transforms.Compose(
-        #     [
-        #         transforms.Resize(tuple(hparams.dataset.image_size)),
-        #         transforms.ToTensor(),
-        #         transforms.CenterCrop(tuple(hparams.dataset.crop_size)),
-        #     ]
-        # )
 
         self.inv_normalize = transforms.Normalize(
             mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.255], std=[1 / 0.229, 1 / 0.224, 1 / 0.255]
@@ -444,16 +362,18 @@ class PatchcoreLightning(pl.LightningModule):
         features = self(images)
         # features2 = self._model(images)
 
-        embeddings = []
-        for feature in features:
-            m = torch.nn.AvgPool2d(3, 1, 1)
-            embeddings.append(m(feature))
-        # embeddings = self._model(batch["image"])
-        embedding = embedding_concat(embeddings[0], embeddings[1])
-        # TODO:Compare the new view_embedding function with reshape_embedding
-        # self.embedding_list.extend(reshape_embedding(np.array(embedding)))
+        # embeddings = []
+        # for feature in features:
+        #     m = torch.nn.AvgPool2d(3, 1, 1)
+        #     embeddings.append(m(feature))
 
-        return {"embedding": reshape_embedding(embedding)}
+        embeddings = self._model.append_features(features)
+        # embeddings = self._model(batch["image"])
+        # embedding = embedding_concat(embeddings[0], embeddings[1])
+        embedding = concat_layer_embedding(embeddings[0], embeddings[1])
+        embedding = reshape_embedding(embedding)
+
+        return {"embedding": embedding}
 
     def training_epoch_end(self, outputs):
         # total_embeddings = np.array(self.embedding_list)
