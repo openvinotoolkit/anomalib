058bc4bfefc0d8354d910ae72915966f338d0206
blakshma <barath.lakshmanan@intel.com>
2021-07-27 09:27:58 -0700

    addressing type related feedback

--- anomalib/datasets/parser.py
+++ anomalib/datasets/parser.py
@@ -2,30 +2,36 @@
 This script contains parsers for different annotations for object detection task.
     Parsers include pascal-voc,.
 """
+import logging
+from typing import Dict, Union, Any, Optional
+
 from xml.etree import ElementTree
 from lxml import etree
 
 XML_EXT = ".xml"
 ENCODE_METHOD = "utf-8"
 
+logger = logging.getLogger(name="Dataset: Anomaly")
+
 
 class PascalVocReader:
     """
     Data parser for Pascal-VOC labels
     """
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         # shapes type:
-        self.labels = list()
-        self.boxes = list()
+        self.boxes = None
+        self.labels: list = list()
+        self.boxe: list = list()
         self.file_path = file_path
-        self.verified = False
-        self.xml_tree = None
+        self.verified: bool = False
+        self.xml_tree: ElementTree.Element = None
         try:
             self.parse_xml()
         except RuntimeError:
-            pass
+            logger.warning("Incorrect format: Unable to parse xml file.")
 
-    def get_shapes(self):
+    def get_shapes(self) -> Dict[str, Union[list, Any]]:
         """
         Returns:
             annotated bounding boxes and corresponding labels
@@ -33,7 +39,7 @@ class PascalVocReader:
 
         return {"boxes": self.boxes, "labels": self.labels}
 
-    def add_shape(self, label, bnd_box):
+    def add_shape(self, label: str, bnd_box: Optional[ElementTree.Element]):
         """
         Args:
             label: label for target object
@@ -56,15 +62,12 @@ class PascalVocReader:
         assert self.file_path.endswith(XML_EXT), "Unsupported file format"
         parser = etree.XMLParser(encoding=ENCODE_METHOD)
         self.xml_tree = ElementTree.parse(self.file_path, parser=parser).getroot()
-        try:
-            verified = self.xml_tree.attrib["verified"]
-            if verified == "yes":
-                self.verified = True
-        except KeyError:
+        if "verified" in self.xml_tree.attrib and self.xml_tree.attrib["verified"] == "yes":
+            self.verified = True
+        else:
             self.verified = False
 
         for object_iter in self.xml_tree.findall("object"):
             bnd_box = object_iter.find("bndbox")
             label = object_iter.find("name").text
             self.add_shape(label, bnd_box)
-        return True
