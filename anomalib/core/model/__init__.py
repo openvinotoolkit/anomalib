8b8651ee649b3ce290bb2610ca9cb503b92da54c
Samet Akcay <samet.akcay@intel.com>
2021-12-01 17:00:10 +0300

    renamed anomaly_module to anomalib_module

--- anomalib/core/model/__init__.py
+++ anomalib/core/model/__init__.py
@@ -14,6 +14,6 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-from .anomaly_module import AnomalyModule
+from .anomalib_module import AnomalyModule
 
 __all__ = ["AnomalyModule"]
