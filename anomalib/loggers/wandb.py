bf9908de35ee440280e4f33608ac517821a2ff16
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2021-12-06 12:05:37 +0100

    stash wandb changes
    Issues:
    - Multiple image logging does not work

---
+++ anomalib/loggers/wandb.py
@@ -0,0 +1,124 @@
+"""wandb logger with add image interface."""
+
+# Copyright (C) 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+
+from typing import Any, Optional, Union
+
+import numpy as np
+from matplotlib.figure import Figure
+from pytorch_lightning.loggers.wandb import WandbLogger
+from pytorch_lightning.utilities import rank_zero_only
+
+import wandb
+
+from .base import ImageLoggerBase
+
+
+class AnomalibWandbLogger(ImageLoggerBase, WandbLogger):
+    """Logger for wandb.
+
+    Adds interface for `add_image` in the logger rather than calling the experiment object.
+    The rest is same as the WandbLogger provided by PyTorch Lightning and the doc string
+    for which is reproduced below:
+
+    Log using `Weights and Biases <https://www.wandb.com/>`_.
+
+    Install it with pip:
+
+    .. code-block:: bash
+
+        $ pip install wandb
+
+    Args:
+        name: Display name for the run.
+        save_dir: Path where data is saved (wandb dir by default).
+        offline: Run offline (data can be streamed later to wandb servers).
+        id: Sets the version, mainly used to resume a previous run.
+        version: Same as id.
+        anonymous: Enables or explicitly disables anonymous logging.
+        project: The name of the project to which this run will belong.
+        log_model: Save checkpoints in wandb dir to upload on W&B servers.
+        prefix: A string to put at the beginning of metric keys.
+        experiment: WandB experiment object. Automatically set when creating a run.
+        **kwargs: Arguments passed to :func:`wandb.init` like `entity`, `group`, `tags`, etc.
+
+    Raises:
+        ImportError:
+            If required WandB package is not installed on the device.
+        MisconfigurationException:
+            If both ``log_model`` and ``offline``is set to ``True``.
+
+    Example:
+        >>> from pytorch_lightning.loggers import WandbLogger
+        >>> from pytorch_lightning import Trainer
+        >>> wandb_logger = WandbLogger()
+        >>> trainer = Trainer(logger=wandb_logger)
+
+    Note: When logging manually through `wandb.log` or `trainer.logger.experiment.log`,
+    make sure to use `commit=False` so the logging step does not increase.
+
+    See Also:
+        - `Tutorial <https://colab.research.google.com/drive/16d1uctGaw2y9KhGBlINNTsWpmlXdJwRW?usp=sharing>`__
+          on how to use W&B with PyTorch Lightning
+        - `W&B Documentation <https://docs.wandb.ai/integrations/lightning>`__
+
+    """
+
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        save_dir: Optional[str] = None,
+        offline: Optional[bool] = False,
+        id: Optional[str] = None,  # kept to match wandb init pylint: disable=redefined-builtin
+        anonymous: Optional[bool] = None,
+        version: Optional[str] = None,
+        project: Optional[str] = None,
+        log_model: Optional[bool] = False,
+        experiment=None,
+        prefix: Optional[str] = "",
+        sync_step: Optional[bool] = None,
+        **kwargs
+    ) -> None:
+        super().__init__(
+            name=name,
+            save_dir=save_dir,
+            offline=offline,
+            id=id,
+            anonymous=anonymous,
+            version=version,
+            project=project,
+            log_model=log_model,
+            experiment=experiment,
+            prefix=prefix,
+            sync_step=sync_step,
+            **kwargs
+        )
+
+    @rank_zero_only
+    def add_image(self, image: Union[np.ndarray, Figure], name: Optional[str] = None, **kwargs: Any):
+        """Interface to add image to wandb logger.
+
+        Args:
+            image (Union[np.ndarray, Figure]): Image to log
+            name (Optional[str]): The tag of the image
+            kwargs: Accepts only `global_step` (int). The step at which to log the image.
+                If not provided, wandb will assume that each image is added at a different step.
+        """
+        step = None
+        if "global_step" in kwargs:
+            step = kwargs.get("global_step")
+
+        image = wandb.Image(image, caption=name)
+        wandb.log({"Predictions": image}, step=step)
