99cf2baa233561eb5647493f3eeb8ab2f2d08814
Samet <samet.akcay@intel.com>
2021-12-06 06:15:01 -0700

    Added CLI before instantiate method

--- anomalib/cli/cli.py
+++ anomalib/cli/cli.py
@@ -1,9 +1,17 @@
 """Anomalib CLI."""
 
+from typing import List
+
 from pytorch_lightning import LightningDataModule, LightningModule
+from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.utilities.cli import LightningArgumentParser, LightningCLI
 
-from anomalib.core.callbacks.tiler import TilerCallback
+from anomalib.core.callbacks import (
+    SaveToCSVCallback,
+    TilerCallback,
+    TimerCallback,
+    VisualizerCallback,
+)
 
 
 class AnomalibCLI(LightningCLI):
@@ -16,7 +24,8 @@ class AnomalibCLI(LightningCLI):
             subclass_mode_model=True,
             subclass_mode_data=True,
             seed_everything_default=0,
-            save_config_overwrite=False,
+            save_config_callback=None,
+            # save_config_overwrite=False,
             run=False,
         )
 
@@ -26,9 +35,29 @@ class AnomalibCLI(LightningCLI):
         Args:
             parser (LightningArgumentParser): Lightning Argument Parser.
         """
+        parser.add_argument("--save_to_csv", type=bool, default=False, help="Save results to a CSV")
+        parser.add_argument("--save_images", type=bool, default=True, help="Flag to save output images locally.")
         # NOTE: MyPy gives the following error:
         # Argument 1 to "add_lightning_class_args" of "LightningArgumentParser"
         # has incompatible type "Type[TilerCallback]"; expected "Union[Type[Trainer],
         # Type[LightningModule], Type[LightningDataModule]]"  [arg-type]
         parser.add_lightning_class_args(TilerCallback, "tiling")  # type: ignore
         parser.set_defaults({"tiling.enable": False})
+
+    def before_instantiate_classes(self) -> None:
+        """Modify the configuration to properly instantiate classes."""
+        callbacks: List[Callback] = []
+
+        # Add timing to the pipeline.
+        callbacks.append(TimerCallback())
+
+        if self.config["save_to_csv"]:
+            callbacks.append(SaveToCSVCallback())
+
+        # Visualization.
+        if self.config["save_images"]:
+            callbacks.append(VisualizerCallback(loggers=["local"]))
+
+        self.config["trainer"]["callbacks"] = callbacks
+
+        print("done.")
