0cdb25ca99aa4b37b324edb4850fa496204cd7dc
Dick Ameln <dick.ameln@intel.com>
2021-12-15 20:26:47 +0100

    improve logic of training stats metric

--- anomalib/core/metrics/training_stats.py
+++ anomalib/core/metrics/training_stats.py
@@ -14,16 +14,14 @@ class TrainingStats(Metric):
         self.anomaly_maps = []
         self.anomaly_scores = []
 
-        self.image_mean = Tensor(0)
-        self.image_std = Tensor(0)
-        self.pixel_mean = Tensor(0)
-        self.pixel_std = Tensor(0)
-
-        self.persistent(mode=True)
+        self.image_mean = torch.empty(0)
+        self.image_std = torch.empty(0)
+        self.pixel_mean = torch.empty(0)
+        self.pixel_std = torch.empty(0)
 
     # pylint: disable=arguments-differ
     def update(
-        self, anomaly_maps: Optional[Tensor] = None, anomaly_scores: Optional[Tensor] = None
+        self, anomaly_scores: Optional[Tensor] = None, anomaly_maps: Optional[Tensor] = None
     ) -> None:  # type: ignore
         """Update the precision-recall curve metric."""
         if anomaly_maps is not None:
@@ -33,23 +31,18 @@ class TrainingStats(Metric):
 
     def compute(self) -> Dict[str, Tensor]:
         """Compute stats."""
-        anomaly_maps = torch.vstack(self.anomaly_maps)
-        anomaly_maps = torch.log(anomaly_maps).cpu()
-        if len(self.anomaly_scores) == 0:
-            anomaly_scores = anomaly_maps.reshape(anomaly_maps.shape[0], -1).max(axis=1).values
-        else:
-            anomaly_scores = torch.hstack(self.anomaly_scores)
+        metrics = {}
 
-        self.image_mean = anomaly_scores.mean()
-        self.image_std = anomaly_scores.std()
+        anomaly_scores = torch.log(torch.hstack(self.anomaly_scores))
 
-        # per pixel stats
-        self.pixel_mean = anomaly_maps.mean(dim=0).squeeze()
-        self.pixel_std = anomaly_maps.std(dim=0).squeeze()
+        metrics["image_mean"] = anomaly_scores.mean()
+        metrics["image_std"] = anomaly_scores.std()
 
-        metrics = dict(
-            image_mean=self.image_mean, image_std=self.image_std, pixel_mean=self.pixel_mean, pixel_std=self.pixel_std
-        )
-        del self.anomaly_scores
-        del self.anomaly_maps
+        if self.anomaly_maps:
+            anomaly_maps = torch.vstack(self.anomaly_maps)
+            anomaly_maps = torch.log(anomaly_maps).cpu()
+
+            metrics["pixel_mean"] = anomaly_maps.mean(dim=0).squeeze()
+            metrics["pixel_std"] = anomaly_maps.std(dim=0).squeeze()
+
         return metrics
