ff26959402641085a7d29c364aba3dc58043661b
Samet <samet.akcay@intel.com>
2021-12-17 03:29:46 -0700

    added issue to hard-coded version in setup.py

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="anomalib",
+    # TODO: https://github.com/openvinotoolkit/anomalib/issues/36
     version="0.2.1",
     author="Intel OpenVINO",
     author_email="help@openvino.intel.com",
