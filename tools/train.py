2ad990595781ce02dfbe9cf1e25c5a82f4623803
Samet Akcay <samet.akcay@intel.com>
2022-01-28 06:22:32 -0700

    Fixed padim training

--- tools/train.py
+++ tools/train.py
@@ -60,7 +60,7 @@ def train():
     trainer = Trainer(**config.trainer, logger=logger, callbacks=callbacks)
     trainer.fit(model=model, datamodule=datamodule)
     # TODO: https://github.com/openvinotoolkit/anomalib/issues/62
-    trainer.validate(model=model, datamodule=datamodule)
+    # trainer.validate(model=model, datamodule=datamodule)
     trainer.test(model=model, datamodule=datamodule)
 
 
