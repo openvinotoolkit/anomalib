a40395ed96c49a9400f22740159d5dfa5ef07112
Dick Ameln <dick.ameln@intel.com>
2022-02-02 22:20:50 +0100

    add force_cpu option to threshold independent metrics

--- anomalib/core/metrics/auroc.py
+++ anomalib/core/metrics/auroc.py
@@ -7,6 +7,16 @@ from torchmetrics.functional import auc
 class AUROC(ROC):
     """Area under the ROC curve."""
 
+    def __init__(self, force_cpu: bool = False, **kwargs):
+        super().__init__(**kwargs)
+        self.force_cpu = force_cpu
+
+    def update(self, preds: Tensor, target: Tensor) -> None:  # type: ignore
+        """Update stats for ROC."""
+        if self.force_cpu:
+            self.cpu()
+        super().update(preds.flatten(), target.flatten())
+
     def compute(self) -> Tensor:
         """First compute ROC curve, then compute area under the curve.
 
