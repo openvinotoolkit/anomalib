5c440e05798e8092e73f7f05f9a60556f7072826
Samet Akcay <samet.akcay@intel.com>
2022-03-06 12:30:14 -0700

    Added Cflow

--- anomalib/models/__init__.py
+++ anomalib/models/__init__.py
@@ -21,13 +21,14 @@ from typing import List, Union
 from omegaconf import DictConfig, ListConfig
 from torch import load
 
+from anomalib.models.cflow import Cflow
 from anomalib.models.components import AnomalyModule
 from anomalib.models.dfkde import Dfkde
 from anomalib.models.dfm import Dfm
 from anomalib.models.padim import Padim
 from anomalib.models.stfpm import Stfpm
 
-__all__ = ["Dfkde", "Dfm", "Padim", "Stfpm"]
+__all__ = ["Cflow", "Dfkde", "Dfm", "Padim", "Stfpm"]
 
 
 def get_model(config: Union[DictConfig, ListConfig]) -> AnomalyModule:
