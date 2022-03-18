fed9d91cd16e0ed71074db1716ad2d93f0c90c22
André Aquilina <dre.aquil.94@gmail.com>
2022-03-18 23:38:34 +0000

    Change to read version from init file.

--- setup.py
+++ setup.py
@@ -85,8 +85,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="anomalib",
-    # TODO: https://github.com/openvinotoolkit/anomalib/issues/36
-    version="0.2.4",
+    version=VERSION,
     author="Intel OpenVINO",
     author_email="help@openvino.intel.com",
     description="anomalib - Anomaly Detection Library",
