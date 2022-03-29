8726444e5ced1df58d41fef16e6648790195e243
Samet Akcay <samet.akcay@intel.com>
2022-03-29 07:30:08 -0700

    Update pre-commit

---
+++ anomalib/models/components/freia/__init__.py
@@ -0,0 +1,9 @@
+"""Framework for Easily Invertible Architectures.
+
+Module to construct invertible networks with pytorch, based on a graph
+structure of operations.
+"""
+
+# Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
+# SPDX-License-Identifier: MIT
+#
---
+++ anomalib/models/components/freia/modules/__init__.py
@@ -0,0 +1,5 @@
+"""Modules."""
+
+# Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
+# SPDX-License-Identifier: MIT
+#
