da73fbedb06f2838534383a4125ffa10e9f781cb
Samet Akcay <samet.akcay@intel.com>
2022-03-29 07:56:58 -0700

    Relative imports

--- anomalib/models/components/freia/framework/__init__.py
+++ anomalib/models/components/freia/framework/__init__.py
@@ -3,3 +3,7 @@
 # Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
 # SPDX-License-Identifier: MIT
 #
+
+from .sequence_inn import SequenceINN
+
+__all__ = ["SequenceINN"]
--- anomalib/models/components/freia/modules/__init__.py
+++ anomalib/models/components/freia/modules/__init__.py
@@ -3,3 +3,8 @@
 # Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
 # SPDX-License-Identifier: MIT
 #
+
+from .all_in_one_block import AllInOneBlock
+from .base import InvertibleModule
+
+__all__ = ["AllInOneBlock", "InvertibleModule"]
