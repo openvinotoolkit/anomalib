3f1d4fdf1dc578dad1371a27bd08c6c4fc9e5c7f
Samet Akcay <samet.akcay@intel.com>
2022-03-30 02:43:05 -0700

    Added github link to the init

--- anomalib/models/components/freia/__init__.py
+++ anomalib/models/components/freia/__init__.py
@@ -2,6 +2,8 @@
 
 Module to construct invertible networks with pytorch, based on a graph
 structure of operations.
+
+Link to the original repo: https://github.com/VLL-HD/FrEIA
 """
 
 # Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
