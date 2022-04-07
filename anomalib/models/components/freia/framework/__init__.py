83b4d543b4e4efa94a38e06a2efc425b1fa90fc2
Samet Akcay <samet.akcay@intel.com>
2022-04-07 12:51:22 +0100

    Remove `freia` as dependency and include it in `anomalib/models/components` (#174)
    * Removed FrEIA from the dependencies
    
    * Added soft permutation as an option to the config
    
    * Update pre-commit
    
    * Added modules
    
    * Updated tox.ini to exclude freia packages.
    
    * Relative imports
    
    * Ignore pylint in sequence_inn
    
    * Addressed pydocstyle
    
    * replaced FrEIA with anomalib freia modules
    
    * Add ignores to freia
    
    * Use nncf version inistead
    
    * Added freia to third party programs.
    
    * Bump up pre-commit black
    
    * Address black version and dependency issues
    
    * Added github link to the init
    
    * Added readme file to freia
    
    * removed linting ignores from the modules
    
    * Address Dicks comments

---
+++ anomalib/models/components/freia/framework/__init__.py
@@ -0,0 +1,9 @@
+"""Framework."""
+
+# Copyright (c) 2018-2022 Lynton Ardizzone, Visual Learning Lab Heidelberg.
+# SPDX-License-Identifier: MIT
+#
+
+from .sequence_inn import SequenceINN
+
+__all__ = ["SequenceINN"]
