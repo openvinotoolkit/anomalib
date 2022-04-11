703d229c8319e23ae76b9d74f081a8027f89fb04
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2022-04-11 14:30:43 +0200

    Update patch

--- tests/pre_merge/utils/loggers/test_get_logger.py
+++ tests/pre_merge/utils/loggers/test_get_logger.py
@@ -28,7 +28,7 @@ from anomalib.utils.loggers import (
 )
 
 
-@patch("anomalib.utils.loggers.wandb.wandb")
+@patch("pytorch_lightning.loggers.wandb.WandbLogger")
 def test_get_logger(wandb):
     """Test whether the right logger is returned."""
 
