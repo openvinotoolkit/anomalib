02a0f8173b10fa01f9b190401aad6311d8310196
Samet Akcay <samet.akcay@intel.com>
2022-04-21 11:01:53 +0100

    Revert "🏷  Rename `--model_config_path` to `config` (#246)"
    This reverts commit ee6a112f33bb3d1a8e5202ccb27d1192eda33358.

--- tools/test.py
+++ tools/test.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-import warnings
 from argparse import ArgumentParser, Namespace
 
 from pytorch_lightning import Trainer
@@ -33,21 +32,11 @@ def get_args() -> Namespace:
     """
     parser = ArgumentParser()
     parser.add_argument("--model", type=str, default="stfpm", help="Name of the algorithm to train/test")
-    # --model_config_path will be deprecated in 0.2.8 and removed in 0.2.9
     parser.add_argument("--model_config_path", type=str, required=False, help="Path to a model config file")
-    parser.add_argument("--config", type=str, required=False, help="Path to a model config file")
     parser.add_argument("--weight_file", type=str, default="weights/model.ckpt")
+    parser.add_argument("--openvino", type=bool, default=False)
 
-    args = parser.parse_args()
-    if args.model_config_path is not None:
-        warnings.warn(
-            message="--model_config_path will be deprecated in v0.2.8 and removed in v0.2.9. Use --config instead.",
-            category=DeprecationWarning,
-            stacklevel=2,
-        )
-        args.config = args.model_config_path
-
-    return args
+    return parser.parse_args()
 
 
 def test():
@@ -58,8 +47,9 @@ def test():
     args = get_args()
     config = get_configurable_parameters(
         model_name=args.model,
-        config_path=args.config,
+        model_config_path=args.model_config_path,
         weight_file=args.weight_file,
+        openvino=args.openvino,
     )
 
     datamodule = get_datamodule(config)
