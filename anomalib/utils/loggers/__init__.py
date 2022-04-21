6e08b81e9f8a0d76524ae1a13f7edd78fd714a1f
Samet Akcay <samet.akcay@intel.com>
2022-04-21 07:15:02 -0700

    🗑  Removed console.py annd ➕  add get_console_logger to __init__

--- anomalib/utils/loggers/__init__.py
+++ anomalib/utils/loggers/__init__.py
@@ -14,15 +14,15 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-
+import logging
 import os
-from typing import Iterable, List, Union
+from logging import Logger
+from typing import Iterable, List, Optional, Union
 
 from omegaconf.dictconfig import DictConfig
 from omegaconf.listconfig import ListConfig
 from pytorch_lightning.loggers import CSVLogger, LightningLoggerBase
 
-from .console import get_console_logger
 from .tensorboard import AnomalibTensorBoardLogger
 from .wandb import AnomalibWandbLogger
 
@@ -37,6 +37,35 @@ AVAILABLE_LOGGERS = ["tensorboard", "wandb", "csv"]
 
 class UnknownLogger(Exception):
     """This is raised when the logger option in `config.yaml` file is set incorrectly."""
+
+
+def get_console_logger(name: Optional[str] = None, level: Union[int, str] = logging.INFO) -> Logger:
+    """Get console logger by name.
+
+    Args:
+        name (Optional[str], optional): Logger name. Defaults to None.
+        level (Union[int, str], optional): Logger Level. Defaults to logging.INFO.
+
+    Returns:
+        Logger: The expected logger.
+    """
+
+    if name is None:
+        name = __name__
+
+    if isinstance(level, str):
+        level = logging.getLevelName(level)
+
+    format_string = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(format=format_string, level=level)
+    logger = logging.getLogger(name)
+
+    # Set Pytorch Lightning logs to have a the consistent formatting with anomalib.
+    for handler in logging.getLogger("pytorch_lightning").handlers:
+        handler.setFormatter(logging.Formatter(format_string))
+        handler.setLevel(level)
+
+    return logger
 
 
 def get_experiment_logger(
