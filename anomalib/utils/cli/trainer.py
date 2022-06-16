9ca510d2ea6831c35cd7c3d8cd64ca44f8891d12
Samet Akcay <samet.akcay@intel.com>
2022-06-15 22:27:32 -0700

    Add anomalib CLI entrypoint

---
+++ anomalib/utils/cli/trainer.py
@@ -0,0 +1,15 @@
+"""Trainer."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from anomalib.utils.cli import AnomalibCLI
+
+
+def main() -> None:
+    """Trainer via Anomalib CLI."""
+    AnomalibCLI()
+
+
+if __name__ == "__main__":
+    main()
