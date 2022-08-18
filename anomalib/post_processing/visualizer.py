5c8525f3a10df663e8a7bd59acf33257d39d5e3e
Siddharth Mehta <smehta73@Siddharths-MacBook-Pro.local>
2022-08-18 12:49:27 -0700

    added comet-ml to base requirements

--- anomalib/post_processing/visualizer.py
+++ anomalib/post_processing/visualizer.py
@@ -225,7 +225,8 @@ class ImageGrid:
                 axis.title.set_text(image_dict["title"])
         self.figure.canvas.draw()
         # convert canvas to numpy array to prepare for visualization with opencv
-        img = np.frombuffer(self.figure.canvas.tostring_rgb(), dtype=np.uint8)
-        img = img.reshape(self.figure.canvas.get_width_height()[::-1] + (3,))
+        buf = self.figure.canvas.tostring_rgb()
+        ncols, nrows = self.figure.canvas.get_width_height()
+        img = np.fromstring(buf, dtype=np.uint8).reshape(nrows, ncols, 3)
         plt.close(self.figure)
         return img
