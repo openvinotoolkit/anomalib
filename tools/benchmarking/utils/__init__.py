8e0687be63e393b33311f324b852fa0cab4684bf
sherpan <siddharthm2350@gmail.com>
2022-09-07 13:03:39 -0700

    comet benchmarking enabled

--- tools/benchmarking/utils/__init__.py
+++ tools/benchmarking/utils/__init__.py
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .convert import convert_to_openvino
-from .metrics import upload_to_wandb, write_metrics
+from .metrics import upload_to_comet, upload_to_wandb, write_metrics
 
-__all__ = ["convert_to_openvino", "write_metrics", "upload_to_wandb"]
+__all__ = ["convert_to_openvino", "write_metrics", "upload_to_comet", "upload_to_wandb"]
