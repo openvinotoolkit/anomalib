5d9fbca429e8ae2065c99809e32c97198d2701d5
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2022-09-11 12:59:31 +0200

    add unit tests for mvtec loco

--- tests/pre_merge/datasets/test_dataset.py
+++ tests/pre_merge/datasets/test_dataset.py
@@ -6,10 +6,42 @@ import numpy as np
 import pytest
 
 from anomalib.config import update_input_size_config
-from anomalib.data import BTech, Folder, MVTec, get_datamodule
+from anomalib.data import BTech, Folder, MVTec, MVTecLOCO, get_datamodule, mvtec_loco
 from anomalib.pre_processing.transforms import Denormalize, ToNumpy
 from tests.helpers.config import get_test_configurable_parameters
 from tests.helpers.dataset import TestDataset, get_dataset_path
+
+
+@pytest.fixture
+def mvtec_loco_data_module(request):
+    datamodule = MVTecLOCO(
+        root=get_dataset_path(dataset="MVTecLOCO"),
+        category="pushpins",
+        task="segmentation",
+        image_size=(100, 170),  # 10x smaller than original
+        train_batch_size=1,
+        test_batch_size=1,
+        num_workers=0,
+        imread_strategy=request.param.get("imread_strategy", mvtec_loco.IMREAD_STRATEGY_ONTHEFLY),
+        transform_config_train=None,
+        transform_config_val=None,
+    )
+    datamodule.prepare_data()
+    datamodule.setup("fit")
+    datamodule.setup("validate")
+    datamodule.setup("test")
+
+    yield datamodule
+
+
+MVTEC_LOCO_PARAMS_ALL_IMREAD_STRATEGIES = [
+    {"imread_strategy": mvtec_loco.IMREAD_STRATEGY_ONTHEFLY},
+    {"imread_strategy": mvtec_loco.IMREAD_STRATEGY_PRELOAD},
+]
+
+MVTEC_LOCO_PARAMS_ALL_IMREAD_ONTHEFLY_ONLY = [
+    {"imread_strategy": mvtec_loco.IMREAD_STRATEGY_ONTHEFLY},
+]
 
 
 @pytest.fixture(autouse=True)
@@ -45,7 +77,7 @@ def btech_data_module():
     return datamodule
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=False)
 def folder_data_module():
     """Create Folder Data Module."""
     root = get_dataset_path(dataset="bottle")
@@ -72,6 +104,47 @@ def folder_data_module():
 def data_sample(mvtec_data_module):
     _, data = next(enumerate(mvtec_data_module.train_dataloader()))
     return data
+
+
+class TestMVTecLOCODataModule:
+    """Test MVTec LOCO Data Module."""
+
+    @pytest.mark.parametrize("mvtec_loco_data_module", MVTEC_LOCO_PARAMS_ALL_IMREAD_STRATEGIES, indirect=True)
+    def test_sizes(self, mvtec_loco_data_module):
+        """test_mvtec_datamodule [summary]"""
+
+        _, train_data_sample = next(enumerate(mvtec_loco_data_module.train_dataloader()))
+        _, val_data_sample = next(enumerate(mvtec_loco_data_module.val_dataloader()))
+        _, test_data_sample = next(enumerate(mvtec_loco_data_module.test_dataloader()))
+
+        for split, data_sample in zip(["train", "val", "test"], [train_data_sample, val_data_sample, test_data_sample]):
+            image = data_sample["image"]
+            assert image.shape == (1, 3, 100, 170), f"Image shape is wrong for {split} split"
+
+    @pytest.mark.parametrize("mvtec_loco_data_module", MVTEC_LOCO_PARAMS_ALL_IMREAD_STRATEGIES, indirect=True)
+    def test_val_and_test_dataloaders_has_mask_and_gt(self, mvtec_loco_data_module):
+        """Test Validation and Test dataloaders should return more things than just the image."""
+        _, val_data = next(enumerate(mvtec_loco_data_module.val_dataloader()))
+        _, test_data = next(enumerate(mvtec_loco_data_module.test_dataloader()))
+        expected_keys = sorted(
+            ["image", "image_path", "mask", "masks", "mask_paths", "label", "super_anotype", "anotype"]
+        )
+        assert expected_keys == sorted(val_data.keys()), "Validation dataloader keys are wrong"
+        assert expected_keys == sorted(test_data.keys()), "Test dataloader keys are wrong"
+
+    @pytest.mark.parametrize("mvtec_loco_data_module", MVTEC_LOCO_PARAMS_ALL_IMREAD_ONTHEFLY_ONLY, indirect=True)
+    def test_non_overlapping_splits(self, mvtec_loco_data_module):
+        """This test ensures that the train and test splits generated are non-overlapping."""
+
+        train_paths = set(mvtec_loco_data_module.train_data.samples["image_path"].values)
+        val_paths = set(mvtec_loco_data_module.val_data.samples["image_path"].values)
+        test_paths = set(mvtec_loco_data_module.test_data.samples["image_path"].values)
+
+        assert len(set.intersection(train_paths, val_paths)) == 0, "Found train and val split contamination"
+
+        assert len(set.intersection(train_paths, test_paths)) == 0, "Found train and test split contamination"
+
+        assert len(set.intersection(test_paths, val_paths)) == 0, "Found val and test split contamination"
 
 
 class TestMVTecDataModule:
