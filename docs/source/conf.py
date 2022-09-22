0c82490d2a9e7ce5e1633b3f74296fece762e302
Samet <samet.akcay@intel.com>
2022-09-22 03:35:21 -0700

    Update conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,12 +43,7 @@ extensions = [
     "sphinxemoji.sphinxemoji",
     "sphinx.ext.autosectionlabel",
     "myst_parser",
-    "autoapi.extension",
 ]
-
-autoapi_dirs = ["../../anomalib"]
-autoapi_root = "api"
-autoapi_type = "python"
 
 autosummary_generate = True
 autodoc_member_order = "groupwise"
@@ -73,13 +68,19 @@ templates_path = ["_templates"]
 # exclude_patterns = []
 
 
-# -- Options for HTML output -------------------------------------------------
-
+# -- Options for HTML output ------------------------------------------------- #
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "alabaster"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = "furo"
+html_static_path = ["_static"]
+html_logo = "images/logos/anomalib-icon.png"
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
 # Sphinx will add “permalinks” for each heading and description environment as paragraph signs that
 #  become visible when the mouse hovers over them.
