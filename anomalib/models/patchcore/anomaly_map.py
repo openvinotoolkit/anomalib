6b5f2629d6a7e88187856caa05c2bb19876e7b8e
Ashwin Vaidya <ashwin.vaidya@intel.com>
2022-09-26 20:34:01 +0200

    Minor refactor (#587)
    * 🛠 Fix PatchCore image-level score computation (#580)
    
    * fix patchcore image-level score computation
    
    * docstring and comment
    
    * remove default value for n_neighbors
    
    * torch.Tensor -> Tensor
    
    * Minor refactor
    
    Co-authored-by: Dick Ameln <dick.ameln@intel.com>
    Co-authored-by: Ashwin Vaidya <ashwinitinvaidya@gmail.com>

--- anomalib/models/patchcore/anomaly_map.py
+++ anomalib/models/patchcore/anomaly_map.py
@@ -8,7 +8,7 @@ from typing import Tuple, Union
 import torch
 import torch.nn.functional as F
 from omegaconf import ListConfig
-from torch import nn
+from torch import Tensor, nn
 
 from anomalib.models.components import GaussianBlur2d
 
@@ -26,67 +26,32 @@ class AnomalyMapGenerator(nn.Module):
         kernel_size = 2 * int(4.0 * sigma + 0.5) + 1
         self.blur = GaussianBlur2d(kernel_size=(kernel_size, kernel_size), sigma=(sigma, sigma), channels=1)
 
-    def compute_anomaly_map(self, patch_scores: torch.Tensor, feature_map_shape: torch.Size) -> torch.Tensor:
+    def compute_anomaly_map(self, patch_scores: Tensor) -> torch.Tensor:
         """Pixel Level Anomaly Heatmap.
 
         Args:
-            patch_scores (torch.Tensor): Patch-level anomaly scores
-            feature_map_shape (torch.Size): 2-D feature map shape (width, height)
+            patch_scores (Tensor): Patch-level anomaly scores
 
         Returns:
             torch.Tensor: Map of the pixel-level anomaly scores
         """
-        width, height = feature_map_shape
-        batch_size = len(patch_scores) // (width * height)
-
-        anomaly_map = patch_scores[:, 0].reshape((batch_size, 1, width, height))
-        anomaly_map = F.interpolate(anomaly_map, size=(self.input_size[0], self.input_size[1]))
-
+        anomaly_map = F.interpolate(patch_scores, size=(self.input_size[0], self.input_size[1]))
         anomaly_map = self.blur(anomaly_map)
 
         return anomaly_map
 
-    @staticmethod
-    def compute_anomaly_score(patch_scores: torch.Tensor) -> torch.Tensor:
-        """Compute Image-Level Anomaly Score.
-
-        Args:
-            patch_scores (torch.Tensor): Patch-level anomaly scores
-        Returns:
-            torch.Tensor: Image-level anomaly scores
-        """
-        max_scores = torch.argmax(patch_scores[:, 0])
-        confidence = torch.index_select(patch_scores, 0, max_scores)
-        weights = 1 - torch.max(F.softmax(confidence, dim=-1))
-        score = weights * torch.max(patch_scores[:, 0])
-        return score
-
-    def forward(self, **kwargs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, patch_scores: Tensor) -> Tensor:
         """Returns anomaly_map and anomaly_score.
 
-        Expects `patch_scores` keyword to be passed explicitly
-        Expects `feature_map_shape` keyword to be passed explicitly
+        Args:
+            patch_scores (Tensor): Patch-level anomaly scores
 
         Example
         >>> anomaly_map_generator = AnomalyMapGenerator(input_size=input_size)
-        >>> map, score = anomaly_map_generator(patch_scores=numpy_array, feature_map_shape=feature_map_shape)
-
-        Raises:
-            ValueError: If `patch_scores` key is not found
+        >>> map = anomaly_map_generator(patch_scores=patch_scores)
 
         Returns:
-            Tuple[torch.Tensor, torch.Tensor]: anomaly_map, anomaly_score
+            Tensor: anomaly_map
         """
-
-        if "patch_scores" not in kwargs:
-            raise ValueError(f"Expected key `patch_scores`. Found {kwargs.keys()}")
-
-        if "feature_map_shape" not in kwargs:
-            raise ValueError(f"Expected key `feature_map_shape`. Found {kwargs.keys()}")
-
-        patch_scores = kwargs["patch_scores"]
-        feature_map_shape = kwargs["feature_map_shape"]
-
-        anomaly_map = self.compute_anomaly_map(patch_scores, feature_map_shape)
-        anomaly_score = self.compute_anomaly_score(patch_scores)
-        return anomaly_map, anomaly_score
+        anomaly_map = self.compute_anomaly_map(patch_scores)
+        return anomaly_map
