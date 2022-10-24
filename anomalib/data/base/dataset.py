48082877145b23894e73f3efece34fc5a2334a06
Dick Ameln <dick.ameln@intel.com>
2022-10-24 13:11:58 +0200

    fix expected columns

--- anomalib/data/base/dataset.py
+++ anomalib/data/base/dataset.py
@@ -21,7 +21,7 @@ from torch.utils.data import Dataset
 from anomalib.data.utils import read_image
 from anomalib.pre_processing import PreProcessor
 
-_EXPECTED_COlS_CLASSIFICATION = ["image_path", "label", "label_index", "mask_path", "split"]
+_EXPECTED_COlS_CLASSIFICATION = ["image_path", "label", "label_index", "split"]
 _EXPECTED_COLS_SEGMENTATION = _EXPECTED_COlS_CLASSIFICATION + ["mask_path"]
 _EXPECTED_COLS_PERTASK = {
     "classification": _EXPECTED_COlS_CLASSIFICATION,
