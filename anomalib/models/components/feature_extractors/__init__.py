657952333bf86a2adefc5038baec53d1e35f2ea3
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2022-11-16 15:22:06 +0100

    Add FeatureExtractor method for backward compatibility

--- anomalib/models/components/feature_extractors/__init__.py
+++ anomalib/models/components/feature_extractors/__init__.py
@@ -3,8 +3,28 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
+from typing import Any
+
 from .timm import TimmFeatureExtractor
 from .torchfx import TorchFXFeatureExtractor
 from .utils import dryrun_find_featuremap_dims
 
-__all__ = ["TimmFeatureExtractor", "TorchFXFeatureExtractor", "dryrun_find_featuremap_dims"]
+
+class FeatureExtractor:
+    """Compatibility wrapper for the old FeatureExtractor class.
+
+    See :class:`anomalib.models.components.feature_extractors.timm.TimmFeatureExtractor` for more details.
+    """
+
+    def __init__(self, *args, **kwargs):
+        logger = logging.getLogger(__name__)
+        logger.warning("FeatureExtractor is deprecated. Use TimmFeatureExtractor instead.")
+        self.feature_extractor = TimmFeatureExtractor(*args, **kwargs)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        """Calls the TimmFeatureExtractor."""
+        return self.feature_extractor(*args, **kwds)
+
+
+__all__ = ["TimmFeatureExtractor", "TorchFXFeatureExtractor", "dryrun_find_featuremap_dims", "FeatureExtractor"]
