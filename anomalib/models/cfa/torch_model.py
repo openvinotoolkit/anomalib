b105b2af7bd3fe12a3c7485ad1bbdefc87b48c56
Samet <samet.akcay@intel.com>
2022-12-11 21:48:16 -0700

    Step 0: Setup the new refactoring pipeline.

--- anomalib/models/cfa/torch_model.py
+++ anomalib/models/cfa/torch_model.py
@@ -13,10 +13,135 @@ from torch.nn.common_types import _size_2_t
 from torch.utils.data import DataLoader
 from torchvision.models.feature_extraction import create_feature_extractor
 from tqdm import tqdm
+from utils.coordconv import CoordConv2d
 
+from anomalib.models.cfa.utils.metric import *
 from anomalib.models.components import GaussianBlur2d
 
 SUPPORTED_BACKBONES = ("vgg19_bn", "resnet18", "wide_resnet50_2", "efficientnet_b5")
+
+
+# TODO: >>> This is temporary.
+def initialize_weights(m) -> None:
+    torch.manual_seed(0)
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_uniform_(m.weight.data, nonlinearity="relu")
+        if m.bias is not None:
+            nn.init.constant_(m.bias.data, 0)
+
+
+# TODO: <<< This is temporary.
+
+
+class CfaModel(nn.Module):
+    def __init__(self, model, data_loader, cnn, gamma_c, gamma_d, device):
+        super().__init__()
+        self.device = device
+
+        self.C = 0
+        self.nu = 1e-3
+        self.scale = None
+
+        self.gamma_c = gamma_c
+        self.gamma_d = gamma_d
+        self.alpha = 1e-1
+        self.K = 3
+        self.J = 3
+
+        self.r = nn.Parameter(1e-5 * torch.ones(1), requires_grad=True)
+        self.Descriptor = Descriptor(self.gamma_d, cnn).to(device)
+        # TODO: >>> Temporary.
+        self.Descriptor.apply(initialize_weights)
+        # TODO <<< Temporary.
+        self._init_centroid(model, data_loader)
+        self.C = rearrange(self.C, "b c h w -> (b h w) c").detach()
+
+        if self.gamma_c > 1:
+            self.C = self.C.cpu().detach().numpy()
+            self.C = KMeans(n_clusters=(self.scale**2) // self.gamma_c, max_iter=3000).fit(self.C).cluster_centers_
+            self.C = torch.Tensor(self.C).to(device)
+
+        self.C = self.C.transpose(-1, -2).detach()
+        self.C = nn.Parameter(self.C, requires_grad=False)
+
+    def forward(self, p):
+        phi_p = self.Descriptor(p)
+        phi_p = rearrange(phi_p, "b c h w -> b (h w) c")
+
+        features = torch.sum(torch.pow(phi_p, 2), 2, keepdim=True)
+        centers = torch.sum(torch.pow(self.C, 2), 0, keepdim=True)
+        f_c = 2 * torch.matmul(phi_p, (self.C))
+        dist = features + centers - f_c
+        dist = torch.sqrt(dist)
+
+        n_neighbors = self.K
+        dist = dist.topk(n_neighbors, largest=False).values
+
+        dist = (F.softmin(dist, dim=-1)[:, :, 0]) * dist[:, :, 0]
+        dist = dist.unsqueeze(-1)
+
+        score = rearrange(dist, "b (h w) c -> b c h w", h=self.scale)
+
+        loss = 0
+        if self.training:
+            loss = self._soft_boundary(phi_p)
+
+        return loss, score
+
+    def _soft_boundary(self, phi_p):
+        features = torch.sum(torch.pow(phi_p, 2), 2, keepdim=True)
+        centers = torch.sum(torch.pow(self.C, 2), 0, keepdim=True)
+        f_c = 2 * torch.matmul(phi_p, (self.C))
+        dist = features + centers - f_c
+        n_neighbors = self.K + self.J
+        dist = dist.topk(n_neighbors, largest=False).values
+
+        score = dist[:, :, : self.K] - self.r**2
+        L_att = (1 / self.nu) * torch.mean(torch.max(torch.zeros_like(score), score))
+
+        score = self.r**2 - dist[:, :, self.J :]
+        L_rep = (1 / self.nu) * torch.mean(torch.max(torch.zeros_like(score), score - self.alpha))
+
+        loss = L_att + L_rep
+
+        return loss
+
+    def _init_centroid(self, model, data_loader):
+        for i, (x, _, _) in enumerate(tqdm(data_loader)):
+            x = x.to(self.device)
+            p = model(x)
+            self.scale = p[0].size(2)
+            phi_p = self.Descriptor(p)
+            self.C = ((self.C * i) + torch.mean(phi_p, dim=0, keepdim=True).detach()) / (i + 1)
+
+
+class Descriptor(nn.Module):
+    def __init__(self, gamma_d, cnn):
+        super(Descriptor, self).__init__()
+        self.cnn = cnn
+        if cnn == "wide_resnet50_2":
+            dim = 1792
+            self.layer = CoordConv2d(dim, dim // gamma_d, 1)
+        elif cnn == "resnet18":
+            dim = 448
+            self.layer = CoordConv2d(dim, dim // gamma_d, 1)
+        elif cnn == "efficientnet_b5":
+            dim = 568
+            self.layer = CoordConv2d(dim, 2 * dim // gamma_d, 1)
+        elif cnn == "vgg19_bn":
+            dim = 1280
+            self.layer = CoordConv2d(dim, dim // gamma_d, 1)
+
+    def forward(self, p):
+        sample = None
+        for o in p:
+            o = F.avg_pool2d(o, 3, 1, 1) / o.size(1) if self.cnn == "efficientnet_b5" else F.avg_pool2d(o, 3, 1, 1)
+            sample = (
+                o if sample is None else torch.cat((sample, F.interpolate(o, sample.size(2), mode="bilinear")), dim=1)
+            )
+
+        phi_p = self.layer(sample)
+        return phi_p
 
 
 def get_feature_extractor(backbone: str, device: Optional[torch.device] = None):
@@ -39,306 +164,3 @@ def get_feature_extractor(backbone: str, device: Optional[torch.device] = None):
         feature_extractor = feature_extractor.to(device)
 
     return feature_extractor
-
-
-# TODO: >>> This is temporary.
-def initialize_weights(m) -> None:
-    torch.manual_seed(0)
-    if isinstance(m, nn.Conv2d):
-        nn.init.kaiming_uniform_(m.weight.data, nonlinearity="relu")
-        if m.bias is not None:
-            nn.init.constant_(m.bias.data, 0)
-
-
-# TODO: <<< This is temporary.
-
-
-class CfaModel(nn.Module):
-    def __init__(
-        self, data_loader: DataLoader, backbone: str, gamma_c: int, gamma_d: int, device: torch.device
-    ) -> None:
-        super().__init__()
-        self.gamma_c = gamma_c
-        self.gamma_d = gamma_d
-        self.device = device
-
-        self.input_size: Tuple[int, int]
-        self.scale: int
-
-        self.num_nearest_neighbors = 3
-        self.num_hard_negative_features = 3
-
-        self.feature_extractor = get_feature_extractor(backbone=backbone, device=device)
-        self.descriptor = Descriptor(self.gamma_d, backbone).to(device)
-        self.r = torch.ones(1, requires_grad=True) * 1e-5
-
-        # TODO: >>> Temporary.
-        self.descriptor.apply(initialize_weights)
-        # TODO <<< Temporary.
-
-        self.memory_bank = self._init_centroid(data_loader)
-
-    def _init_centroid(self, data_loader: DataLoader) -> Tensor:
-        """Initialize the Centroid of the Memory Bank.
-
-        Args:
-            model (nn.Module): Feature Extractor Model.
-            data_loader (DataLoader): Train Dataloader.
-
-        Returns:
-            Tensor: Memory Bank.
-        """
-        memory_bank: Tensor = torch.tensor(0, requires_grad=False)
-        with torch.no_grad():
-            for i, (batch, _, _) in enumerate(tqdm(data_loader)):
-                batch = batch.to(self.device)
-                features = self.feature_extractor(batch)
-                # TODO: >>> Conversion from dict to list.
-                features = [val for val in features.values()]
-                # TODO <<< Conversion from dict to list.
-
-                # TODO <<< Find a better place for these.
-                self.input_size = (batch.size(2), batch.size(3))
-                self.scale = features[0].size(2)
-                # TODO >>> Find a better place for these.
-
-                oriented_features = self.descriptor(features)
-                memory_bank = ((memory_bank * i) + oriented_features.mean(dim=0, keepdim=True)) / (i + 1)
-
-        memory_bank = rearrange(memory_bank, "b c h w -> (b h w) c")
-
-        if self.gamma_c > 1:
-            k_means = KMeans(n_clusters=(self.scale**2) // self.gamma_c, max_iter=3000)
-            cluster_centers = k_means.fit(memory_bank.cpu()).cluster_centers_
-            memory_bank = torch.tensor(cluster_centers, requires_grad=False).to(self.device)
-
-        memory_bank = rearrange(memory_bank, "h w -> w h")
-
-        return memory_bank
-
-    def compute_distance(self, target_oriented_features: Tensor) -> Tensor:
-        """Compute distance using target oriented features.
-
-        Args:
-            target_oriented_features (Tensor): Target oriented features computed
-                using the descriptor.
-
-        Returns:
-            Tensor: Distance tensor.
-        """
-        features = target_oriented_features.pow(2).sum(dim=2, keepdim=True)
-        centers = self.memory_bank.pow(2).sum(dim=0, keepdim=True)
-        f_c = 2 * torch.matmul(target_oriented_features, (self.memory_bank))
-        distance = features + centers - f_c
-        return distance
-
-    def compute_loss(self, distance: Tensor) -> Tensor:
-        """Compute the CFA loss.
-
-        Args:
-            distance (Tensor): Distance computed using target oriented features.
-
-        Returns:
-            Tensor: CFA loss.
-        """
-        num_neighbors = self.num_nearest_neighbors + self.num_hard_negative_features
-        distance = distance.topk(num_neighbors, largest=False).values
-
-        score = distance[:, :, : self.num_nearest_neighbors] - (self.r**2).to(self.device)
-        L_att = torch.mean(torch.max(torch.zeros_like(score), score))
-
-        score = (self.r**2).to(self.device) - distance[:, :, self.num_hard_negative_features :]
-        L_rep = torch.mean(torch.max(torch.zeros_like(score), score - 0.1))
-
-        loss = (L_att + L_rep) * 1000
-
-        return loss
-
-    def compute_score(self, distance: Tensor) -> Tensor:
-        """Compute score based on the distance.
-
-        Args:
-            distance (Tensor): Distance tensor computed using target oriented
-                features.
-
-        Returns:
-            Tensor: Score value.
-        """
-        distance = torch.sqrt(distance)
-
-        n_neighbors = self.num_nearest_neighbors
-        distance = distance.topk(n_neighbors, largest=False).values
-
-        distance = (F.softmin(distance, dim=-1)[:, :, 0]) * distance[:, :, 0]
-        distance = distance.unsqueeze(-1)
-
-        score = rearrange(distance, "b (h w) c -> b c h w", h=self.scale)
-        return score.detach()
-
-    def compute_anomaly_map(self, score: Tensor) -> Tensor:
-        """Compute anomaly map based on the score.
-
-        Args:
-            score (Tensor): Score tensor.
-
-        Returns:
-            Tensor: Anomaly map.
-        """
-        anomaly_map = score.mean(dim=1, keepdim=True)
-        anomaly_map = F.interpolate(anomaly_map, size=self.input_size, mode="bilinear", align_corners=False)
-
-        gaussian_blur = GaussianBlur2d(sigma=4).to(score.device)
-        anomaly_map = gaussian_blur(anomaly_map)
-        return anomaly_map
-
-    def forward(self, input: Tensor) -> Tensor:
-        """Forward pass of the CFA model.
-
-        Args:
-            input (Tensor): Input batch to the model.
-
-        Returns:
-            Tensor: Loss or the score values depending on train/eval modes.
-        """
-        self.feature_extractor.eval()
-        features = self.feature_extractor(input)
-
-        target_features = self.descriptor(features)
-        target_features = rearrange(target_features, "b c h w -> b (h w) c")
-
-        distance = self.compute_distance(target_features)
-
-        if self.training:
-            output = self.compute_loss(distance)
-        else:
-            score = self.compute_score(distance)
-            output = self.compute_anomaly_map(score)
-        return output
-
-
-class Descriptor(nn.Module):
-    def __init__(self, gamma_d: int, backbone: str) -> None:
-        super(Descriptor, self).__init__()
-
-        self.backbone = backbone
-        if self.backbone not in SUPPORTED_BACKBONES:
-            raise ValueError(f"Supported backbones are {SUPPORTED_BACKBONES}. Got {self.backbone} instead.")
-
-        backbone_dims = {"vgg19_bn": 1280, "resnet18": 448, "wide_resnet50_2": 1792, "efficientnet_b5": 568}
-        dim = backbone_dims[backbone]
-        out_channels = 2 * dim // gamma_d if backbone == "efficientnet_b5" else dim // gamma_d
-
-        self.layer = CoordConv2d(in_channels=dim, out_channels=out_channels, kernel_size=1)
-
-    def forward(self, features: Union[List[Tensor], Dict[str, Tensor]]) -> Tensor:
-        """Forward pass."""
-        if isinstance(features, dict):
-            features = [values for values in features.values()]
-
-        patch_features: Optional[Tensor] = None
-        for i in features:
-            i = F.avg_pool2d(i, 3, 1, 1) / i.size(1) if self.backbone == "efficientnet_b5" else F.avg_pool2d(i, 3, 1, 1)
-            patch_features = (
-                i
-                if patch_features is None
-                else torch.cat((patch_features, F.interpolate(i, patch_features.size(2), mode="bilinear")), dim=1)
-            )
-
-        target_oriented_features = self.layer(patch_features)
-        return target_oriented_features
-
-
-class CoordConv2d(nn.Conv2d):
-    """CoordConv layer as in the paper.
-
-    Link to the paper: https://arxiv.org/abs/1807.03247
-    Link to the PyTorch implementation: https://github.com/walsvid/CoordConv
-    """
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: _size_2_t,
-        stride: _size_2_t = 1,
-        padding: Union[str, _size_2_t] = 0,
-        dilation: _size_2_t = 1,
-        groups: int = 1,
-        bias: bool = True,
-        with_r: bool = False,
-    ) -> None:
-        super().__init__(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            dilation=dilation,
-            groups=groups,
-            bias=bias,
-        )
-        # AddCoord layer.
-        self.add_coords = AddCoords(with_r)
-
-        # Create conv layer on top of add_coords layer.
-        self.conv2d = nn.Conv2d(
-            in_channels=in_channels + 2 + int(with_r),  # 2 for rank-2 tensor, 1 for r if with_r
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            dilation=dilation,
-            groups=groups,
-            bias=bias,
-        )
-
-    def forward(self, input: Tensor) -> Tensor:
-        out = self.add_coords(input)
-        out = self.conv2d(out)
-        return out
-
-
-class AddCoords(nn.Module):
-    """Add coords to a tensor.
-
-    Link to the paper: https://arxiv.org/abs/1807.03247
-    Link to the PyTorch implementation: https://github.com/walsvid/CoordConv
-    """
-
-    def __init__(self, with_r: bool = False) -> None:
-        super().__init__()
-        self.with_r = with_r
-
-    def forward(self, input: Tensor) -> Tensor:
-        # NOTE: This is a modified version of the original implementation, which only supports rank 2 tensors.
-        batch, _, x_dim, y_dim = input.shape
-        xx_ones = torch.ones([1, 1, 1, y_dim], dtype=torch.int32)
-        yy_ones = torch.ones([1, 1, 1, x_dim], dtype=torch.int32)
-
-        xx_range = torch.arange(x_dim, dtype=torch.int32)
-        yy_range = torch.arange(y_dim, dtype=torch.int32)
-        xx_range = xx_range[None, None, :, None]
-        yy_range = yy_range[None, None, :, None]
-
-        xx_channel = torch.matmul(xx_range, xx_ones)
-        yy_channel = torch.matmul(yy_range, yy_ones)
-
-        # Transpose y
-        yy_channel = yy_channel.permute(0, 1, 3, 2)
-
-        xx_channel = xx_channel.float() / (x_dim - 1)
-        yy_channel = yy_channel.float() / (y_dim - 1)
-
-        xx_channel = xx_channel * 2 - 1
-        yy_channel = yy_channel * 2 - 1
-
-        xx_channel = xx_channel.repeat(batch, 1, 1, 1).to(input.device)
-        yy_channel = yy_channel.repeat(batch, 1, 1, 1).to(input.device)
-
-        out = torch.cat([input, xx_channel, yy_channel], dim=1)
-
-        if self.with_r:
-            rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
-            out = torch.cat([out, rr], dim=1)
-
-        return out
---
+++ anomalib/models/cfa/train.py
@@ -0,0 +1,234 @@
+import argparse
+import random
+import warnings
+
+import datasets.mvtec as mvtec
+import torch
+import torch.optim as optim
+from cnn.efficientnet import EfficientNet as effnet
+from cnn.resnet import resnet18, wide_resnet50_2
+from cnn.vgg import vgg19_bn
+from datasets.mvtec import MVTecDataset
+from torch.utils.data import DataLoader
+from torch_model import CfaModel
+from utils.cfa import *
+from utils.metric import *
+from utils.visualizer import *
+
+warnings.filterwarnings("ignore", category=UserWarning)
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("CFA configuration")
+    parser.add_argument("--data_path", type=str, default="/home/sakcay/projects/anomalib/datasets/MVTec/")
+    parser.add_argument("--save_path", type=str, default="./mvtec_result")
+    parser.add_argument("--Rd", type=bool, default=False)
+    parser.add_argument(
+        "--cnn",
+        type=str,
+        choices=["resnet18", "wide_resnet50_2", "efficientnet_b5", "vgg19_bn"],
+        default="wide_resnet50_2",
+    )
+    parser.add_argument("--resize", type=int, choices=[224, 256], default=224)
+    parser.add_argument("--size", type=int, choices=[224, 256], default=224)
+    parser.add_argument("--gamma_c", type=int, default=1)
+    parser.add_argument("--gamma_d", type=int, default=1)
+
+    parser.add_argument("--class_name", type=str, default="zipper")
+
+    return parser.parse_args()
+
+
+def run():
+    seed = 1024
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if use_cuda:
+        torch.cuda.manual_seed_all(seed)
+
+    args = parse_args()
+    class_names = [random.choice(mvtec.CLASS_NAMES)]
+    print(f"Class name: {class_names[0]}")
+
+    total_roc_auc = []
+    total_pixel_roc_auc = []
+    total_pixel_pro_auc = []
+
+    # fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+    # fig_img_rocauc = ax[0]
+    # fig_pixel_rocauc = ax[1]
+
+    for class_name in class_names:
+        best_img_roc1 = -1
+        best_img_roc2 = -1
+        best_pxl_roc1 = -1
+        best_pxl_roc2 = -1
+        best_pxl_pro1 = -1
+        best_pxl_pro2 = -1
+        print(" ")
+        print("%s | newly initialized..." % class_name)
+
+        train_dataset = MVTecDataset(
+            dataset_path=args.data_path,
+            class_name=class_name,
+            resize=args.resize,
+            cropsize=args.size,
+            is_train=True,
+            wild_ver=args.Rd,
+        )
+
+        test_dataset = MVTecDataset(
+            dataset_path=args.data_path,
+            class_name=class_name,
+            resize=args.resize,
+            cropsize=args.size,
+            is_train=False,
+            wild_ver=args.Rd,
+        )
+
+        train_loader = DataLoader(
+            dataset=train_dataset,
+            batch_size=4,
+            pin_memory=True,
+            shuffle=True,
+            drop_last=True,
+        )
+
+        test_loader = DataLoader(
+            dataset=test_dataset,
+            batch_size=4,
+            pin_memory=True,
+        )
+
+        if args.cnn == "wide_resnet50_2":
+            feature_extractor1 = wide_resnet50_2(pretrained=True, progress=True)
+        elif args.cnn == "resnet18":
+            feature_extractor1 = resnet18(pretrained=True, progress=True)
+        elif args.cnn == "efficientnet_b5":
+            feature_extractor1 = effnet.from_pretrained("efficientnet-b5")
+        elif args.cnn == "vgg19_bn":
+            feature_extractor1 = vgg19_bn(pretrained=True, progress=True)
+
+        feature_extractor1 = feature_extractor1.to(device)
+        feature_extractor1.eval()
+
+        cfa1 = DSVDD(feature_extractor1, train_loader, args.cnn, args.gamma_c, args.gamma_d, device)
+        cfa2 = CfaModel(feature_extractor1, train_loader, args.cnn, args.gamma_c, args.gamma_d, device)
+
+        cfa1 = cfa1.to(device)
+        cfa2 = cfa2.to(device)
+
+        epochs = 1
+        params1 = [{"params": cfa1.parameters()}]
+        params2 = [{"params": cfa2.parameters()}]
+        optimizer1 = optim.AdamW(params=params1, lr=1e-3, weight_decay=5e-4, amsgrad=True)
+        optimizer2 = optim.AdamW(params=params2, lr=1e-3, weight_decay=5e-4, amsgrad=True)
+
+        for epoch in tqdm(range(epochs), "%s -->" % (class_name)):
+            r"TEST PHASE"
+
+            test_imgs = list()
+            gt_mask_list = list()
+            gt_list = list()
+            heatmaps1 = None
+            heatmaps2 = None
+
+            cfa1.train()
+            cfa2.train()
+            for (batch, _, _) in train_loader:
+                optimizer1.zero_grad()
+                optimizer2.zero_grad()
+                features1 = feature_extractor1(batch.to(device))
+
+                loss1, _ = cfa1(features1)
+                loss2, _ = cfa2(features1)
+                loss1.backward(retain_graph=True)
+                loss2.backward(retain_graph=True)
+                optimizer1.step()
+                optimizer2.step()
+                print(f"loss1: {loss1}, loss2: {loss2}")
+
+            cfa1.eval()
+            cfa2.eval()
+            for batch, y, mask in test_loader:
+                test_imgs.extend(batch.cpu().detach().numpy())
+                gt_list.extend(y.cpu().detach().numpy())
+                gt_mask_list.extend(mask.cpu().detach().numpy())
+
+                features1 = feature_extractor1(batch.to(device))
+                _, score1 = cfa1(features1)
+                _, score2 = cfa2(features1)
+                heatmap1 = score1.cpu().detach()
+                heatmap2 = score2.cpu().detach()
+                heatmap1 = torch.mean(heatmap1, dim=1)
+                heatmap2 = torch.mean(heatmap2, dim=1)
+                heatmaps1 = torch.cat((heatmaps1, heatmap1), dim=0) if heatmaps1 != None else heatmap1
+                heatmaps2 = torch.cat((heatmaps2, heatmap2), dim=0) if heatmaps2 != None else heatmap2
+
+            heatmaps1 = upsample(heatmaps1, size=batch.size(2), mode="bilinear")
+            heatmaps2 = upsample(heatmaps2, size=batch.size(2), mode="bilinear")
+            heatmaps1 = gaussian_smooth(heatmaps1, sigma=4)
+            heatmaps2 = gaussian_smooth(heatmaps2, sigma=4)
+
+            gt_mask = np.asarray(gt_mask_list)
+            scores1 = rescale(heatmaps1)
+            scores2 = rescale(heatmaps2)
+
+            # threshold1 = get_threshold(gt_mask, scores1)
+            # threshold2 = get_threshold(gt_mask, scores2)
+
+            r"Image-level AUROC"
+            fpr1, tpr1, img_roc_auc1 = cal_img_roc(scores1, gt_list)
+            fpr2, tpr2, img_roc_auc2 = cal_img_roc(scores2, gt_list)
+            best_img_roc1 = img_roc_auc1 if img_roc_auc1 > best_img_roc1 else best_img_roc1
+            best_img_roc2 = img_roc_auc2 if img_roc_auc2 > best_img_roc2 else best_img_roc2
+
+            # fig_img_rocauc.plot(fpr1, tpr1, label="%s img_ROCAUC: %.3f" % (class_name, img_roc_auc1))
+
+            r"Pixel-level AUROC"
+            fpr1, tpr1, per_pixel_rocauc1 = cal_pxl_roc(gt_mask, scores1)
+            fpr2, tpr2, per_pixel_rocauc2 = cal_pxl_roc(gt_mask, scores2)
+            best_pxl_roc1 = per_pixel_rocauc1 if per_pixel_rocauc1 > best_pxl_roc1 else best_pxl_roc1
+            best_pxl_roc2 = per_pixel_rocauc2 if per_pixel_rocauc2 > best_pxl_roc2 else best_pxl_roc2
+
+            r"Pixel-level AUPRO"
+            per_pixel_proauc1 = cal_pxl_pro(gt_mask, scores1)
+            per_pixel_proauc2 = cal_pxl_pro(gt_mask, scores2)
+            best_pxl_pro1 = per_pixel_proauc1 if per_pixel_proauc1 > best_pxl_pro1 else best_pxl_pro1
+            best_pxl_pro2 = per_pixel_proauc2 if per_pixel_proauc2 > best_pxl_pro2 else best_pxl_pro2
+
+            print(f"img-roc1: {img_roc_auc1:.3f} | img-roc2: {img_roc_auc2:.3f}")
+            print(f"pix-roc1: {per_pixel_rocauc1:.3f} | pix-roc2: {per_pixel_rocauc2:.3f}")
+            print(f"pix-pro1: {per_pixel_proauc1:.3f} | pix-pro2: {per_pixel_proauc2:.3f}")
+
+        # print("image ROCAUC: %.3f" % best_img_roc1)
+        # print("pixel ROCAUC: %.3f" % best_pxl_roc1)
+        # print("pixel ROCAUC: %.3f" % best_pxl_pro1)
+
+        total_roc_auc.append(best_img_roc1)
+        total_pixel_roc_auc.append(best_pxl_roc1)
+        total_pixel_pro_auc.append(best_pxl_pro1)
+
+        # fig_pixel_rocauc.plot(fpr1, tpr1, label="%s ROCAUC: %.3f" % (class_name, per_pixel_rocauc1))
+        # save_dir = args.save_path + "/" + f"pictures_{args.cnn}"
+        # os.makedirs(save_dir, exist_ok=True)
+        # plot_fig(test_imgs, scores1, gt_mask_list, threshold1, save_dir, class_name)
+
+    # print("Average ROCAUC: %.3f" % np.mean(total_roc_auc))
+    # fig_img_rocauc.title.set_text("Average image ROCAUC: %.3f" % np.mean(total_roc_auc))
+    # fig_img_rocauc.legend(loc="lower right")
+
+    # print("Average pixel ROCUAC: %.3f" % np.mean(total_pixel_roc_auc))
+    # fig_pixel_rocauc.title.set_text("Average pixel ROCAUC: %.3f" % np.mean(total_pixel_roc_auc))
+    # fig_pixel_rocauc.legend(loc="lower right")
+
+    # print("Average pixel PROUAC: %.3f" % np.mean(total_pixel_pro_auc))
+
+    # fig.tight_layout()
+    # fig.savefig(os.path.join(args.save_path, "roc_curve.png"), dpi=100)
+
+
+if __name__ == "__main__":
+    run()
