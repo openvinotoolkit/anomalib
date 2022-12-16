2a7e92af098ba2488c3cae71169d5d8bfd632a2a
Dick Ameln <dick.ameln@intel.com>
2022-12-16 09:37:55 +0100

    fix cyclic import

--- anomalib/data/utils/transform.py
+++ anomalib/data/utils/transform.py
@@ -9,7 +9,7 @@ from typing import Optional, Tuple, Union
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-from anomalib.data.utils import get_image_height_and_width
+from anomalib.data.utils.image import get_image_height_and_width
 
 logger = logging.getLogger(__name__)
 
