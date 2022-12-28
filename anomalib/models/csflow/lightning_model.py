bb544c2019605668f5b5bf22957379740528ad9d
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2022-12-28 15:50:41 +0100

    Migrate configs for csflow

--- anomalib/models/csflow/lightning_model.py
+++ anomalib/models/csflow/lightning_model.py
@@ -7,10 +7,8 @@ https://arxiv.org/pdf/2110.02855.pdf
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple
 
-import torch
-from omegaconf import DictConfig, ListConfig
 from pytorch_lightning.utilities.cli import MODEL_REGISTRY
 from torch import Tensor
 
@@ -21,7 +19,7 @@ from .torch_model import CsFlowModel
 
 logger = logging.getLogger(__name__)
 
-__all__ = ["Csflow", "CsflowLightning"]
+__all__ = ["Csflow"]
 
 
 @MODEL_REGISTRY
@@ -33,7 +31,6 @@ class Csflow(AnomalyModule):
         n_coupling_blocks (int): Number of coupling blocks in the model.
         cross_conv_hidden_channels (int): Number of hidden channels in the cross convolution.
         clamp (int): Clamp value for glow layer.
-        num_channels (int): Number of channels in the model.
     """
 
     def __init__(
@@ -42,7 +39,6 @@ class Csflow(AnomalyModule):
         cross_conv_hidden_channels: int,
         n_coupling_blocks: int,
         clamp: int,
-        num_channels: int,
     ):
         super().__init__()
         self.model: CsFlowModel = CsFlowModel(
@@ -50,7 +46,6 @@ class Csflow(AnomalyModule):
             cross_conv_hidden_channels=cross_conv_hidden_channels,
             n_coupling_blocks=n_coupling_blocks,
             clamp=clamp,
-            num_channels=num_channels,
         )
         self.loss = CsFlowLoss()
 
@@ -83,42 +78,3 @@ class Csflow(AnomalyModule):
         batch["anomaly_maps"] = anomaly_maps
         batch["pred_scores"] = anomaly_scores
         return batch
-
-
-class CsflowLightning(Csflow):
-    """Fully Convolutional Cross-Scale-Flows for Image-based Defect Detection.
-
-    Args:
-        hprams (Union[DictConfig, ListConfig]): Model params
-    """
-
-    def __init__(self, hparams: Union[DictConfig, ListConfig]):
-        super().__init__(
-            input_size=hparams.model.input_size,
-            n_coupling_blocks=hparams.model.n_coupling_blocks,
-            cross_conv_hidden_channels=hparams.model.cross_conv_hidden_channels,
-            clamp=hparams.model.clamp,
-            num_channels=3,
-        )
-        self.hparams: Union[DictConfig, ListConfig]  # type: ignore
-        self.save_hyperparameters(hparams)
-
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        """Configures optimizers.
-
-        Note:
-            This method is used for the existing CLI.
-            When PL CLI is introduced, configure optimizers method will be
-                deprecated, and optimizers will be configured from either
-                config.yaml file or from CLI.
-
-        Returns:
-            Optimizer: Adam optimizer
-        """
-        return torch.optim.Adam(
-            self.parameters(),
-            lr=self.hparams.model.lr,
-            eps=self.hparams.model.eps,
-            weight_decay=self.hparams.model.weight_decay,
-            betas=(0.5, 0.9),
-        )
