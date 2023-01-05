9f85ff7b4194edc20d306beca85460a97810220f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 19:55:29 +0000

    Adding tarfile member sanitization to extractall()

--- anomalib/data/mvtec.py
+++ anomalib/data/mvtec.py
@@ -390,7 +390,29 @@ class MVTec(LightningDataModule):
 
             logger.info("Extracting the dataset.")
             with tarfile.open(zip_filename) as tar_file:
-                tar_file.extractall(self.root)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar_file, self.root)
 
             logger.info("Cleaning the tar file")
             (zip_filename).unlink()
