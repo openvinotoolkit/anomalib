bb8c30f3c48f3b72cd07378e99436601f8e504b5
Samet <samet.akcay@intel.com>
2023-03-20 13:36:02 +0000

    Add base depth datamodule tests

--- tests/unit/data/base/__init__.py
+++ tests/unit/data/base/__init__.py
@@ -4,8 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .test_datamodule import _TestAnomalibDataModule
+from .test_depth import _TestAnomalibDepthDatamodule
 from .test_image import _TestAnomalibImageDatamodule
 from .test_video import _TestAnomalibVideoDatamodule
 
 
-__all__ = ["_TestAnomalibDataModule", "_TestAnomalibImageDatamodule", "_TestAnomalibVideoDatamodule"]
+__all__ = [
+    "_TestAnomalibDataModule",
+    "_TestAnomalibDepthDatamodule",
+    "_TestAnomalibImageDatamodule",
+    "_TestAnomalibVideoDatamodule",
+]
---
+++ tests/unit/data/base/test_depth.py
@@ -0,0 +1,57 @@
+"""Unit Tests - Base Depth Datamodules."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from anomalib.data import AnomalibDataModule
+import random
+from .test_datamodule import _TestAnomalibDataModule
+
+
+class _TestAnomalibDepthDatamodule(_TestAnomalibDataModule):
+    def test_get_item_returns_correct_keys_and_shapes(self, datamodule: AnomalibDataModule) -> None:
+        """Test that the datamodule __getitem__ returns the correct keys and shapes."""
+
+        # Randomly select a subset of the dataset.
+        subsets = ["train", "val", "test"]
+        random_subset = random.choice(subsets)
+
+        # Get the dataloader.
+        dataloader = getattr(datamodule, f"{random_subset}_dataloader")()
+
+        # Get the first batch.
+        batch = next(iter(dataloader))
+
+        # Check that the batch has the correct keys.
+        expected_keys = {"image_path", "depth_path", "label", "image", "depth_image"}
+
+        if dataloader.dataset.task in ("detection", "segmentation"):
+            expected_keys |= {"mask_path", "mask"}
+
+            if dataloader.dataset.task == "detection":
+                expected_keys |= {"boxes"}
+        # expected_eval_keys = expected_train_keys | {"mask_path", "mask"}
+
+        # if random_subset == "train":
+        #     expected_keys = expected_train_keys
+        # else:
+        #     if dataloader.dataset.task == "detection":
+        #         expected_keys = expected_eval_keys | {"boxes"}
+        #     else:
+        #         expected_keys = expected_eval_keys
+
+        assert batch.keys() == expected_keys
+
+        # Check that the batch has the correct shape.
+        assert len(batch["image_path"]) == 4
+        assert len(batch["depth_path"]) == 4
+        assert batch["image"].shape == (4, 3, 256, 256)
+        assert batch["depth_image"].shape == (4, 3, 256, 256)
+        assert batch["label"].shape == (4,)
+
+        # TODO: Detection task should return bounding boxes.
+        # if dataloader.dataset.task == "detection":
+        #     assert batch["boxes"].shape == (4, 4)
+
+        if dataloader.dataset.task in ("detection", "segmentation"):
+            assert batch["mask"].shape == (4, 256, 256)
