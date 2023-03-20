7576679bed849075e77ec47df1af8355d5497bfd
Samet <samet.akcay@intel.com>
2023-03-20 13:36:18 +0000

    Add MVTec3D datamodule tests

---
+++ tests/unit/data/test_mvtec3d.py
@@ -0,0 +1,29 @@
+"""Unit Tests - MVTec3D Datamodule."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from anomalib.data import MVTec3D, TaskType
+import pytest
+from .base import _TestAnomalibDepthDatamodule
+
+
+class TestMVTec3D(_TestAnomalibDepthDatamodule):
+    """MVTec Datamodule Unit Tests."""
+
+    @pytest.fixture
+    def datamodule(self, task_type: TaskType) -> MVTec3D:
+        # Create and prepare the dataset
+        _datamodule = MVTec3D(
+            root="./datasets/MVTec3D",
+            category="bagel",
+            task=task_type,
+            image_size=256,
+            train_batch_size=4,
+            eval_batch_size=4,
+            num_workers=0,
+        )
+        _datamodule.prepare_data()
+        _datamodule.setup()
+
+        return _datamodule
