ec08ae1508c26f1dd9d7aaf9876bbbf9eed12481
Samet <samet.akcay@intel.com>
2023-03-22 21:52:54 +0000

    Add dummy data and models

--- tests/helpers/data/__init__.py
+++ tests/helpers/data/__init__.py
@@ -2,3 +2,9 @@
 
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+
+
+from .dummy_numeric_datamodule import DummyNumericDataModule
+from .dummy_tensor_datamodule import DummyTensorDataModule
+
+__all__ = ["DummyNumericDataModule", "DummyTensorDataModule"]
---
+++ tests/helpers/models/__init__.py
@@ -0,0 +1,9 @@
+"""Tests - Model Helpers."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .dummy_model import DummyAnomalibModule, DummyModel
+
+__all__ = ["DummyAnomalibModule", "DummyModel"]
