ece4942c498c70c5afe9768b60b2c2d29e834241
Samet <samet.akcay@intel.com>
2023-03-23 13:32:39 +0000

    Speed up test_synthetic_data

--- tests/unit/data/test_synthetic_data.py
+++ tests/unit/data/test_synthetic_data.py
@@ -8,87 +8,100 @@ from pathlib import Path
 
 import pytest
 
+from anomalib.data import AnomalibDataset, TaskType
 from anomalib.data.folder import FolderDataset
 from anomalib.data.synthetic import SyntheticAnomalyDataset
-from anomalib.data.utils import get_transforms
+from anomalib.data.utils import get_transforms, random_split
 from tests.helpers.data import get_dataset_path
 
 
-def get_folder_dataset():
-    """Create Folder Dataset."""
-    root = get_dataset_path(dataset="bottle")
-    transform = get_transforms(image_size=(256, 256))
+@pytest.fixture(autouse=True)
+def sample_dataset() -> AnomalibDataset:
+    """Creates a folder dataset that is subsampled for faster testing.
+
+    Returns:
+        AnomalibDataset: Subsampled folder dataset.
+    """
+    # Create a folder dataset.
     dataset = FolderDataset(
-        task="segmentation",
-        transform=transform,
-        root=root,
+        root=get_dataset_path(dataset="bottle"),
         normal_dir="good",
         abnormal_dir="broken_large",
         mask_dir="ground_truth/broken_large",
         split="train",
+        task=TaskType.SEGMENTATION,
+        transform=get_transforms(image_size=256),
     )
     dataset.setup()
 
-    return dataset
+    # Subsample the dataset to make it faster.
+    _, _sample_dataset = random_split(dataset, split_ratio=0.01, label_aware=True)
+
+    return _sample_dataset
 
 
 @pytest.fixture(autouse=True)
-def make_synthetic_dataset():
-    """Create synthetic anomaly dataset from folder dataset."""
+def synthetic_dataset(sample_dataset: AnomalibDataset) -> SyntheticAnomalyDataset:
+    """Creates a synthetic dataset from a folder dataset.
 
-    def make():
-        folder_dataset = get_folder_dataset()
-        synthetic_dataset = SyntheticAnomalyDataset.from_dataset(folder_dataset)
-        return synthetic_dataset
+    Args:
+        sample_dataset (AnomalibDataset): Sampled folder dataset.
 
-    return make
+    Returns:
+        SyntheticAnomalyDataset: Synthetic dataset.
+    """
+    return SyntheticAnomalyDataset.from_dataset(sample_dataset)
 
 
 @pytest.fixture(autouse=True)
-def synthetic_dataset_from_samples():
-    """Create synthetic anomaly dataset by passing a samples dataframe."""
-    folder_dataset = get_folder_dataset()
-    transform = get_transforms(image_size=(256, 256))
-    synthetic_dataset = SyntheticAnomalyDataset(
-        task=folder_dataset.task, transform=transform, source_samples=folder_dataset.samples
+def synthetic_dataset_from_samples(sample_dataset: AnomalibDataset) -> SyntheticAnomalyDataset:
+    """Creates a synthetic dataset from a folder dataset.
+
+    Args:
+        sample_dataset (AnomalibDataset): Sampled folder dataset.
+
+    Returns:
+        SyntheticAnomalyDataset: Synthetic dataset.
+    """
+    _synthetic_dataset = SyntheticAnomalyDataset(
+        task=sample_dataset.task,
+        transform=get_transforms(image_size=256),
+        source_samples=sample_dataset.samples,
     )
-    return synthetic_dataset
+    return _synthetic_dataset
 
 
-def test_create_synthetic_dataset(make_synthetic_dataset):
+def test_create_synthetic_dataset(synthetic_dataset: SyntheticAnomalyDataset) -> None:
     """Tests if the image and mask files listed in the synthetic dataset exist."""
-    synthetic_dataset = make_synthetic_dataset()
     assert all(Path(path).exists() for path in synthetic_dataset.samples.image_path)
     assert all(Path(path).exists() for path in synthetic_dataset.samples.mask_path)
 
 
-def test_create_from_dataset(synthetic_dataset_from_samples):
+def test_create_from_dataset(synthetic_dataset_from_samples: SyntheticAnomalyDataset) -> None:
     """Tests if the image and mask files listed in the synthetic dataset exist, when instantiated from samples df."""
     synthetic_dataset = synthetic_dataset_from_samples
     assert all(Path(path).exists() for path in synthetic_dataset.samples.image_path)
     assert all(Path(path).exists() for path in synthetic_dataset.samples.mask_path)
 
 
-def test_cleanup(make_synthetic_dataset):
+def test_cleanup(synthetic_dataset: SyntheticAnomalyDataset) -> None:
     """Tests if the temporary directory is cleaned up when the instance is deleted."""
-    synthetic_dataset = make_synthetic_dataset()
-    root = synthetic_dataset.root
-    del synthetic_dataset
+    synthetic_dataset_cp = copy(synthetic_dataset)
+    root = synthetic_dataset_cp.root
+    del synthetic_dataset_cp
     assert not root.exists()
 
 
-def test_copy(make_synthetic_dataset):
+def test_copy(synthetic_dataset: SyntheticAnomalyDataset) -> None:
     """Tests if the dataset is copied correctly, and files still exist after original instance is deleted."""
-    synthetic_dataset = make_synthetic_dataset()
     synthetic_dataset_cp = copy(synthetic_dataset)
     assert all(synthetic_dataset.samples == synthetic_dataset_cp.samples)
     del synthetic_dataset
     assert synthetic_dataset_cp.root.exists()
 
 
-def test_deepcopy(make_synthetic_dataset):
+def test_deepcopy(synthetic_dataset: SyntheticAnomalyDataset) -> None:
     """Tests if the dataset is deep-copied correctly, and files still exist after original instance is deleted."""
-    synthetic_dataset = make_synthetic_dataset()
     synthetic_dataset_cp = deepcopy(synthetic_dataset)
     assert all(synthetic_dataset.samples == synthetic_dataset_cp.samples)
     del synthetic_dataset
