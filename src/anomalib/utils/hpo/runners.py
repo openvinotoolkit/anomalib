486d98715dd232ed1cdb6a8e863a1f1cc452ddf4
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2023-03-23 11:00:21 +0100

    Remove sweep_callbacks
    Note: Breaks the sweep and benchmarking. This is temporary

--- src/anomalib/utils/hpo/runners.py
+++ src/anomalib/utils/hpo/runners.py
@@ -6,19 +6,15 @@
 from __future__ import annotations
 
 import pytorch_lightning as pl
+import wandb
 from comet_ml import Optimizer
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from pytorch_lightning.loggers import CometLogger, WandbLogger
 
-import wandb
 from anomalib.config import update_input_size_config
 from anomalib.data import get_datamodule
 from anomalib.models import get_model
-from anomalib.utils.sweep import (
-    flatten_sweep_params,
-    get_sweep_callbacks,
-    set_in_nested_config,
-)
+from anomalib.utils.sweep import flatten_sweep_params, set_in_nested_config
 
 from .config import flatten_hpo_params
 
@@ -69,12 +65,11 @@ class WandbSweep:
 
         model = get_model(config)
         datamodule = get_datamodule(config)
-        callbacks = get_sweep_callbacks(config)
 
         # Disable saving checkpoints as all checkpoints from the sweep will get uploaded
         config.trainer.enable_checkpointing = False
 
-        trainer = pl.Trainer(**config.trainer, logger=wandb_logger, callbacks=callbacks)
+        trainer = pl.Trainer(**config.trainer, logger=wandb_logger)
         trainer.fit(model, datamodule=datamodule)
 
 
@@ -123,10 +118,9 @@ class CometSweep:
 
             model = get_model(config)
             datamodule = get_datamodule(config)
-            callbacks = get_sweep_callbacks(config)
 
             # Disable saving checkpoints as all checkpoints from the sweep will get uploaded
             config.trainer.enable_checkpointing = False
 
-            trainer = pl.Trainer(**config.trainer, logger=comet_logger, callbacks=callbacks)
+            trainer = pl.Trainer(**config.trainer, logger=comet_logger)
             trainer.fit(model, datamodule=datamodule)
