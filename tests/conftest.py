145e2f45b525ffd0b630be1bac7d3db199376734
Samet <samet.akcay@intel.com>
2023-03-24 10:28:56 +0000

    Set the scope of device fixture as session

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 import torch
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def device() -> torch.device:
     """Returns the device to run the tests on."""
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
