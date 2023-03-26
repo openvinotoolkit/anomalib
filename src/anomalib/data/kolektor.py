25793c85db838d88ea26ae169091560f185dcfe1
Ravindu <ravindumadawalagama@gmail.com>
2023-03-26 23:53:55 +0530

    Add comments for dataset creation

--- src/anomalib/data/kolektor.py
+++ src/anomalib/data/kolektor.py
@@ -19,6 +19,7 @@ from anomalib.data.utils import (
 )
 
 
+# Check if a mask shows defects
 def is_good(path):
     img_arr = imread(path)
     if np.all(img_arr == 0):
@@ -33,10 +34,11 @@ def make_kolektor_dataset(
 ) -> DataFrame:
 
     root = Path(root)
+
+    # Get list of images and masks
     samples_list = [
         (str(root),) + f.parts[-2:] for f in root.glob(r"**/*") if f.suffix == ".jpg"
     ]
-
     masks_list = [
         (str(root),) + f.parts[-2:] for f in root.glob(r"**/*") if f.suffix == ".bmp"
     ]
@@ -44,36 +46,42 @@ def make_kolektor_dataset(
     if not samples_list:
         raise RuntimeError(f"Found 0 images in {root}")
 
+    # Create dataframes
     samples = DataFrame(samples_list, columns=["path", "item", "image_path"])
     masks = DataFrame(masks_list, columns=["path", "item", "image_path"])
 
+    # Modify image_path column by converting to absolute path
     samples["image_path"] = samples.path + "/" + samples.item + "/" + samples.image_path
     masks["image_path"] = masks.path + "/" + masks.item + "/" + masks.image_path
 
+    # Sort samples by image path
     samples = samples.sort_values(by="image_path", ignore_index=True)
     masks = masks.sort_values(by="image_path", ignore_index=True)
 
+    # Add mask paths for sample images
     samples["mask_path"] = masks.image_path.values
 
+    # Use is_good func to configure the label
     samples["label"] = samples["mask_path"].apply(is_good)
-    samples.loc[(samples.label), "label"] = "Good"
+    samples.loc[(samples.label == True), "label"] = "Good"
     samples.loc[(samples.label == False), "label"] = "Bad"
 
+    # Add label indexes
     samples.loc[(samples.label == "Good"), "label_index"] = 0
     samples.loc[(samples.label == "Bad"), "label_index"] = 1
     samples.label_index = samples.label_index.astype(int)
 
-    # samples = samples.sort_values(by=["label", "image_path"], ignore_index=True)
-
+    # Add all 'Bad' samples to test set
     samples.loc[(samples.label == "Bad"), "split"] = "test"
 
+    # Divide 'good' images to train/test on 0.8/0.2 ratio
     train_samples, test_samples = train_test_split(
         samples[samples.label == "Good"], train_size=0.8, test_size=0.2, random_state=42
     )
-
     samples.loc[train_samples.index, "split"] = "train"
     samples.loc[test_samples.index, "split"] = "test"
 
+    # Reorder columns
     samples = samples[
         ["path", "item", "split", "label", "image_path", "mask_path", "label_index"]
     ]
@@ -87,6 +95,7 @@ def make_kolektor_dataset(
               folder follow the same naming convention as the anomalous images in the dataset (e.g. image: '000.png', \
               mask: '000.png' or '000_mask.png')."
 
+    # Get the dataframe for the required split
     if split:
         samples = samples[samples.split == split].reset_index(drop=True)
 
