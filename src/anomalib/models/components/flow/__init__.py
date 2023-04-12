aa4c8b250cbc7c2bc68623595172d72c955d081f
Samet <samet.akcay@intel.com>
2023-04-12 16:45:11 +0100

    Fix tests

---
+++ src/anomalib/models/components/flow/__init__.py
@@ -0,0 +1,8 @@
+"""All In One Block Layer."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from .all_in_one_block import AllInOneBlock
+
+__all__ = ["AllInOneBlock"]
