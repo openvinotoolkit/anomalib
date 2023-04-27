fbbeaac4983715d3435482f18910d42019231f81
Kang Wenjing <wenjing.kang@intel.com>
2023-04-28 00:38:54 +0800

    Add random_2d_simplex to init
    Signed-off-by: Kang Wenjing <wenjing.kang@intel.com>

--- src/anomalib/data/utils/generators/__init__.py
+++ src/anomalib/data/utils/generators/__init__.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from .opensimplex import random_2d_simplex
 from .perlin import random_2d_perlin
 
-__all__ = ["random_2d_perlin"]
+__all__ = ["random_2d_perlin", "random_2d_simplex"]
