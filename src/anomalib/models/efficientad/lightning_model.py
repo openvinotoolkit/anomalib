b6ed5a374719fd081e85ed0ceb6ca2bdf894598e
alexriedel1 <alex.riedel@googlemail.com>
2023-05-05 00:08:26 +0200

    remove eval

--- src/anomalib/models/efficientad/lightning_model.py
+++ src/anomalib/models/efficientad/lightning_model.py
@@ -188,11 +188,9 @@ class EfficientAD(AnomalyModule):
         """
         Calculate the feature map quantiles of the validation dataset and push to the model.
         """
-        if (self.current_epoch + 1) == self.trainer.max_epochs:
-            self.model.eval()
-            if not self.model.is_set(self.model._quantiles):
-                map_norm_quantiles = self.map_norm_quantiles(self.trainer.val_dataloaders[0])
-                self.model._quantiles.update(map_norm_quantiles)
+        if not self.model.is_set(self.model._quantiles):
+            map_norm_quantiles = self.map_norm_quantiles(self.trainer.val_dataloaders[0])
+            self.model._quantiles.update(map_norm_quantiles)
 
     def validation_step(self, batch: dict[str, str | Tensor], *args, **kwargs) -> STEP_OUTPUT:
         """Validation Step of EfficientAD returns anomaly maps for the input image batch
