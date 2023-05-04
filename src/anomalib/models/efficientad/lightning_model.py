67235338aef2a28ffe9adc77be0a8a9196eacb70
alexriedel1 <alex.riedel@googlemail.com>
2023-05-04 23:40:47 +0200

    manual get last epoch

--- src/anomalib/models/efficientad/lightning_model.py
+++ src/anomalib/models/efficientad/lightning_model.py
@@ -157,7 +157,8 @@ class EfficientAD(AnomalyModule):
         )
 
     def on_train_start(self) -> None:
-        """Calculate or load the channel-wise mean and std of the training dataset and set it to the model."""
+        """Calculate or load the channel-wise mean and std of the training dataset and push to the model."""
+
         if not self.model.is_set(self.model._mean_std):
             channel_mean_std = self.teacher_channel_mean_std(self.trainer.train_dataloader)
             self.model._mean_std.update(channel_mean_std)
@@ -183,15 +184,15 @@ class EfficientAD(AnomalyModule):
         self.log("train_loss", loss.item(), on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss}
 
-    def on_train_end(self) -> None:
+    def on_train_epoch_end(self) -> None:
         """
-        Calculate or load the channel-wise mean and std of the training dataset and set it in the model.
-        Calculate the feature map quantiles of the test dataset and set it in the model.
+        Calculate the feature map quantiles of the validation dataset and push to the model.
         """
-        self.model.eval()
-        if not self.model.is_set(self.model._quantiles):
-            map_norm_quantiles = self.map_norm_quantiles(self.trainer.val_dataloaders[0])
-            self.model._quantiles.update(map_norm_quantiles)
+        if (self.current_epoch + 1) == self.trainer.max_epochs:
+            self.model.eval()
+            if not self.model.is_set(self.model._quantiles):
+                map_norm_quantiles = self.map_norm_quantiles(self.trainer.val_dataloaders[0])
+                self.model._quantiles.update(map_norm_quantiles)
 
     def validation_step(self, batch: dict[str, str | Tensor], *args, **kwargs) -> STEP_OUTPUT:
         """Validation Step of EfficientAD returns anomaly maps for the input image batch
