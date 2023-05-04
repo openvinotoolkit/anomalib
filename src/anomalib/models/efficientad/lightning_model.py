02c1167278ba8572cff7b8e642dd921db0d123a8
alexriedel1 <alex.riedel@googlemail.com>
2023-05-05 00:07:56 +0200

    on validation start calculate quantiles

--- src/anomalib/models/efficientad/lightning_model.py
+++ src/anomalib/models/efficientad/lightning_model.py
@@ -182,10 +182,9 @@ class EfficientAD(AnomalyModule):
         self.log("train_ae", loss_ae.item(), on_epoch=True, prog_bar=True, logger=True)
         self.log("train_stae", loss_stae.item(), on_epoch=True, prog_bar=True, logger=True)
         self.log("train_loss", loss.item(), on_epoch=True, prog_bar=True, logger=True)
-                
         return {"loss": loss}
 
-    def on_train_epoch_end(self) -> None:
+    def on_validation_start(self) -> None:
         """
         Calculate the feature map quantiles of the validation dataset and push to the model.
         """
@@ -194,7 +193,6 @@ class EfficientAD(AnomalyModule):
             if not self.model.is_set(self.model._quantiles):
                 map_norm_quantiles = self.map_norm_quantiles(self.trainer.val_dataloaders[0])
                 self.model._quantiles.update(map_norm_quantiles)
-            self.model.train()
 
     def validation_step(self, batch: dict[str, str | Tensor], *args, **kwargs) -> STEP_OUTPUT:
         """Validation Step of EfficientAD returns anomaly maps for the input image batch
