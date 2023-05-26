74774304911f4a26a45448bf6c8391741a1ff3b1
jasonvanzelm <vanzelm@gestalt-robotics.com>
2023-05-26 13:44:30 +0200

    Fix Fastflow ONNX export.

--- src/anomalib/models/fastflow/torch_model.py
+++ src/anomalib/models/fastflow/torch_model.py
@@ -41,10 +41,14 @@ def subnet_conv_func(kernel_size: int, hidden_ratio: float) -> Callable:
 
     def subnet_conv(in_channels: int, out_channels: int) -> nn.Sequential:
         hidden_channels = int(in_channels * hidden_ratio)
+        # setting padding="same" in nn.Conv2d breaks the onnx export so we pad by hand
+        padding = 2 * (kernel_size // 2 - ((1 + kernel_size) % 2), kernel_size // 2)
         return nn.Sequential(
-            nn.Conv2d(in_channels, hidden_channels, kernel_size, padding="same"),
+            nn.ZeroPad2d(padding),
+            nn.Conv2d(in_channels, hidden_channels, kernel_size),
             nn.ReLU(),
-            nn.Conv2d(hidden_channels, out_channels, kernel_size, padding="same"),
+            nn.ZeroPad2d(padding),
+            nn.Conv2d(hidden_channels, out_channels, kernel_size),
         )
 
     return subnet_conv
