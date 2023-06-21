8797b197ff790881a519f574bda93ed97c051f8e
alexriedel1 <alex.riedel@googlemail.com>
2023-06-21 17:49:15 +0200

    fix precommit

--- src/anomalib/data/utils/transform.py
+++ src/anomalib/data/utils/transform.py
@@ -87,10 +87,10 @@ def get_transforms(
             logger.info("Loading transforms from config File")
             transforms_list = []
 
-            if "Resize" not in config.keys():
-                 resize_height, resize_width = get_image_height_and_width(image_size)
-                 transforms_list.append(A.Resize(height=resize_height, width=resize_width, always_apply=True))
-                 logger.info(f"Resize {resize_height, resize_width} added!")
+            if "Resize" not in config.keys() and image_size is not None:
+                resize_height, resize_width = get_image_height_and_width(image_size)
+                transforms_list.append(A.Resize(height=resize_height, width=resize_width, always_apply=True))
+                logger.info(f"Resize {resize_height, resize_width} added!")
 
             for key, value in config.items():
                 if hasattr(A, key):
