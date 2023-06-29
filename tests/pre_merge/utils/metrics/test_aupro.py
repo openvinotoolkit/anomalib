e6fe63e955941b52a3685a74c3a46d2ad8254782
yann-cv <ychene@scortex.io>
2023-06-29 09:49:34 +0200

    improve test by doing 2 different ones (aupro and binned aupro) + renamed few variables in the tests

--- tests/pre_merge/utils/metrics/test_aupro.py
+++ tests/pre_merge/utils/metrics/test_aupro.py
@@ -8,65 +8,76 @@ from tests.helpers.aupro_reference import calculate_au_pro
 
 
 def pytest_generate_tests(metafunc):
-    if metafunc.function is test_pro:
-        labels = [
-            torch.tensor(
+    labels = [
+        torch.tensor(
+            [
                 [
-                    [
-                        [0, 0, 0, 1, 0, 0, 0],
-                    ]
-                    * 400,
+                    [0, 0, 0, 1, 0, 0, 0],
                 ]
-            ),
-            torch.tensor(
+                * 400,
+            ]
+        ),
+        torch.tensor(
+            [
                 [
-                    [
-                        [0, 1, 0, 1, 0, 1, 0],
-                    ]
-                    * 400,
+                    [0, 1, 0, 1, 0, 1, 0],
                 ]
-            ),
-        ]
-        preds = torch.arange(2800) / 2800.0
-        preds = preds.view(1, 1, 400, 7)
+                * 400,
+            ]
+        ),
+    ]
+    preds = torch.arange(2800) / 2800.0
+    preds = preds.view(1, 1, 400, 7)
 
-        preds = [preds, preds]
+    preds = [preds, preds]
 
-        fpr_limit = [1 / 3, 1 / 3]
-        aupro = [torch.tensor(1 / 6), torch.tensor(1 / 6)]
+    fpr_limit = [1 / 3, 1 / 3]
+    expected_aupro = [torch.tensor(1 / 6), torch.tensor(1 / 6)]
 
-        # Also test that per-region aupros are averaged
-        labels.append(torch.cat(labels))
-        preds.append(torch.cat(preds))
-        fpr_limit.append(float(np.mean(fpr_limit)))
-        aupro.append(torch.tensor(np.mean(aupro)))
+    # Also test that per-region aupros are averaged
+    labels.append(torch.cat(labels))
+    preds.append(torch.cat(preds))
+    fpr_limit.append(float(np.mean(fpr_limit)))
+    expected_aupro.append(torch.tensor(np.mean(expected_aupro)))
 
-        thresholds = [
-            torch.linspace(0, 1, steps=50),
-            torch.linspace(0, 1, steps=50),
-        ]
-        vals = list(zip(labels, preds, thresholds, fpr_limit, aupro))
+    thresholds = [
+        torch.linspace(0, 1, steps=200),
+        torch.linspace(0, 1, steps=200),
+        torch.linspace(0, 1, steps=200),
+    ]
 
-        metafunc.parametrize(argnames=("labels", "preds", "thresholds", "fpr_limit", "aupro"), argvalues=vals)
+    if metafunc.function is test_aupro:
+        vals = list(zip(labels, preds, fpr_limit, expected_aupro))
+        metafunc.parametrize(argnames=("labels", "preds", "fpr_limit", "aupro"), argvalues=vals)
+    elif metafunc.function is test_binned_aupro:
+        vals = list(zip(labels, preds, thresholds))
+        metafunc.parametrize(argnames=("labels", "preds", "thresholds"), argvalues=vals)
 
 
-def test_pro(labels, preds, thresholds, fpr_limit, aupro):
-    pro = AUPRO(fpr_limit=fpr_limit)
-    pro.update(preds, labels)
-    computed_aupro = pro.compute()
+def test_aupro(labels, preds, fpr_limit, expected_aupro):
+    aupro = AUPRO(fpr_limit=fpr_limit)
+    aupro.update(preds, labels)
+    computed_aupro = aupro.compute()
 
     tmp_labels = [label.squeeze().numpy() for label in labels]
     tmp_preds = [pred.squeeze().numpy() for pred in preds]
-    ref_pro = torch.tensor(calculate_au_pro(tmp_labels, tmp_preds, integration_limit=fpr_limit)[0], dtype=torch.float)
+    ref_aupro = torch.tensor(calculate_au_pro(tmp_labels, tmp_preds, integration_limit=fpr_limit)[0], dtype=torch.float)
 
     TOL = 0.001
-    assert torch.allclose(computed_aupro, aupro, atol=TOL)
-    assert torch.allclose(computed_aupro, ref_pro, atol=TOL)
-    assert torch.allclose(aupro, ref_pro, atol=TOL)
+    assert torch.allclose(computed_aupro, expected_aupro, atol=TOL)
+    assert torch.allclose(computed_aupro, ref_aupro, atol=TOL)
+    assert torch.allclose(aupro, ref_aupro, atol=TOL)
 
-    binned_pro = AUPRO(fpr_limit=fpr_limit, thresholds=thresholds)
-    binned_pro.update(preds, labels)
-    computed_binned_aupro = binned_pro.compute()
 
-    assert computed_binned_aupro != computed_aupro
-    assert torch.allclose(computed_aupro, aupro, atol=TOL)
+def test_binned_aupro(labels, preds, thresholds):
+    aupro = AUPRO()
+    computed_not_binned_aupro = aupro(preds, labels)
+
+    binned_pro = AUPRO(thresholds=thresholds)
+    computed_binned_aupro = binned_pro(preds, labels)
+
+    TOL = 0.001
+    # with threshold binning the roc curve computed within the metric is more memory efficient
+    # but a bit less accurate. So we check the difference in order to validate the binning effect.
+    assert computed_binned_aupro != computed_not_binned_aupro
+    assert torch.allclose(computed_not_binned_aupro, computed_binned_aupro, atol=TOL)
