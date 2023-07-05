a382be659d93765eb5d241c0dff27c8f5ba500a4
yann-cv <ychene@scortex.io>
2023-07-05 16:53:15 +0200

    add binning tests

---
+++ tests/pre_merge/utils/metrics/test_binning.py
@@ -0,0 +1,14 @@
+from torch import Tensor, linspace, all
+
+from anomalib.utils.metrics.binning import thresholds_between_min_and_max, \
+    thresholds_between_0_and_1
+
+
+def test_thresholds_between_min_and_max():
+    preds = Tensor([1, 10])
+    assert all(thresholds_between_min_and_max(preds, 2) == preds)
+
+
+def test_thresholds_between_0_and_1():
+    expected = Tensor([0, 1])
+    assert all(thresholds_between_0_and_1(2) == expected)
