29a311a3c7b3309d231d659028a416eb2c7dd439
Blaz Rolih <blaz.rolih@gmail.com>
2023-08-03 14:44:19 +0100

    Refactor for dummy base config

--- tests/pre_merge/models/ensemble/test_ensemble_functions.py
+++ tests/pre_merge/models/ensemble/test_ensemble_functions.py
@@ -3,11 +3,10 @@
 from tempfile import TemporaryDirectory
 from typing import List
 
-from omegaconf import OmegaConf
+import pytest
 from torch import Tensor
 
 from anomalib.config import get_configurable_parameters
-from anomalib.data import MVTec
 from anomalib.models.ensemble import EnsembleTiler
 from anomalib.models.ensemble.ensemble_functions import (
     TileCollater,
@@ -22,61 +21,46 @@ from anomalib.models.ensemble.predictions import (
     RescaledEnsemblePredictions,
 )
 from anomalib.utils.callbacks import MinMaxNormalizationCallback
-from tests.helpers.dataset import get_dataset_path
 
 
-mock_config = OmegaConf.create(
-    {
-        "ensemble": {
-            "tiling": {
-                "tile_size": 256,
-                "stride": 256,
-            },
-            "predictions": {"storage": "direct", "rescale_factor": 0.5},
-        },
-        "dataset": {"image_size": 512, "val_split_mode": "same_as_test"},
-        "project": {"path": ""},
-    }
-)
+def get_datamodule(config, task):
+    tiler = EnsembleTiler(config)
+    config.dataset.task = task
 
+    datamodule = get_ensemble_datamodule(config, tiler)
 
-def get_datamodule(task):
-    datamodule = MVTec(
-        root=get_dataset_path(dataset="MVTec"),
-        category="leather",
-        image_size=mock_config.dataset.image_size,
-        train_batch_size=5,
-        eval_batch_size=5,
-        num_workers=0,
-        task=task,
-        test_split_mode="from_dir",
-        val_split_mode="same_as_test",
-    )
     datamodule.prepare_data()
     datamodule.setup()
 
-    tiler = EnsembleTiler(mock_config)
-    datamodule.custom_collate_fn = TileCollater(tiler, (0, 0))
-
     return datamodule
 
+@pytest.fixture
+def get_config():
+    config = get_configurable_parameters(config_path="tests/pre_merge/models/ensemble/dummy_padim_config.yaml")
+    prepare_ensemble_configurable_parameters(
+        ens_config_path="tests/pre_merge/models/ensemble/dummy_ens_config.yaml", config=config
+    )
+    return config
 
 class TestTileCollater:
     """Test tile collater"""
 
-    def test_collate_tile_shape(self):
+    def test_collate_tile_shape(self, get_config):
+        config = get_config
         # datamodule with tile collater
-        datamodule = get_datamodule("segmentation")
+        datamodule = get_datamodule(config, "segmentation")
 
-        tile_size = mock_config.ensemble.tiling.tile_size
+        tile_w, tile_h = config.ensemble.tiling.tile_size
+        batch_size = config.dataset.train_batch_size
 
         batch = next(iter(datamodule.train_dataloader()))
-        assert batch["image"].shape == (5, 3, tile_size, tile_size)
-        assert batch["mask"].shape == (5, tile_size, tile_size)
+        assert batch["image"].shape == (batch_size, 3, tile_w, tile_h)
+        assert batch["mask"].shape == (batch_size, tile_w, tile_h)
 
-    def test_collate_box_data(self):
+    def test_collate_box_data(self, get_config):
+        config = get_config
         # datamodule with tile collater
-        datamodule = get_datamodule("detection")
+        datamodule = get_datamodule(config, "detection")
 
         batch = next(iter(datamodule.train_dataloader()))
 
@@ -88,25 +72,22 @@ class TestTileCollater:
 class TestHelperFunctions:
     """Test other ensemble helper functions"""
 
-    def test_ensemble_config(self):
-        config = get_configurable_parameters(config_path="src/anomalib/models/padim/config.yaml")
-        prepare_ensemble_configurable_parameters(
-            ens_config_path="tests/pre_merge/models/ensemble/dummy_ens_config.yaml", config=config
-        )
+    def test_ensemble_config(self, get_config):
+        config = get_config
 
         assert "ensemble" in config
         assert config.model.input_size == config.ensemble.tiling.tile_size
 
-    def test_ensemble_datamodule(self):
-        config = get_configurable_parameters(config_path="src/anomalib/models/padim/config.yaml")
+    def test_ensemble_datamodule(self, get_config):
+        config = get_config
 
-        tiler = EnsembleTiler(mock_config)
+        tiler = EnsembleTiler(config)
         datamodule = get_ensemble_datamodule(config, tiler)
 
         assert isinstance(datamodule.custom_collate_fn, TileCollater)
 
-    def test_ensemble_prediction_storage_type(self):
-        config = mock_config.copy()
+    def test_ensemble_prediction_storage_type(self, get_config):
+        config = get_config
 
         config.ensemble.predictions.storage = "direct"
         pred_direct = get_prediction_storage(config)
@@ -129,8 +110,9 @@ class TestHelperFunctions:
         assert isinstance(pred_rescale[0], RescaledEnsemblePredictions)
         assert isinstance(pred_rescale[1], RescaledEnsemblePredictions)
 
-    def test_ensemble_prediction_storage_reference(self):
-        config = mock_config.copy()
+    def test_ensemble_prediction_storage_reference(self, get_config):
+        config = get_config
+
         config.dataset.val_split_mode = "same_as_test"
         pred_same = get_prediction_storage(config)
 
@@ -143,11 +125,8 @@ class TestHelperFunctions:
         # if test data is NOT same as validation, storage should be separate object
         assert pred_from[0] is not pred_from[1]
 
-    def test_ensemble_callbacks(self):
-        config = get_configurable_parameters(config_path="src/anomalib/models/padim/config.yaml")
-        prepare_ensemble_configurable_parameters(
-            ens_config_path="tests/pre_merge/models/ensemble/dummy_ens_config.yaml", config=config
-        )
+    def test_ensemble_callbacks(self, get_config):
+        config = get_config
         config.ensemble.post_processing.normalization = "final"
 
         callbacks = get_ensemble_callbacks(config, (0, 0))
