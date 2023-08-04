784767fc2f19a8f354f152aba7f4338cb628118c
Ashwin Vaidya <ashwin.vaidya@intel.com>
2023-08-04 17:24:51 +0200

    🚚 Refactor Benchmarking Script (#1216)
    * New printing stuff
    
    * Remove dead code + address codacy issues
    
    * Refactor try/except + log to comet/wandb during runs
    
    * pre-commit error
    
    * third-party configuration
    
    ---------
    
    Co-authored-by: Ashwin Vaidya <ashwinitinvaidya@gmail.com>

--- tools/benchmarking/utils/__init__.py
+++ tools/benchmarking/utils/__init__.py
@@ -3,6 +3,6 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from .metrics import upload_to_comet, upload_to_wandb, write_metrics
+from .metrics import write_metrics, write_to_comet, write_to_wandb
 
-__all__ = ["write_metrics", "upload_to_comet", "upload_to_wandb"]
+__all__ = ["write_metrics", "write_to_comet", "write_to_wandb"]
