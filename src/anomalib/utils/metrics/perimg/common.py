08d35fa6ec0ccc916c6cf6f6391d190fc2e2a1ec
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2023-09-08 15:59:56 +0200

    fix types in boxplot

--- src/anomalib/utils/metrics/perimg/common.py
+++ src/anomalib/utils/metrics/perimg/common.py
@@ -230,9 +230,9 @@ def _perimg_boxplot_stats(
         records.append(
             dict(
                 statistic=stat_,
-                value=val_,
-                nearest=nearest,
-                imgidx=imgidx,
+                value=float(val_),
+                nearest=float(nearest),
+                imgidx=int(imgidx),
             )
         )
 
