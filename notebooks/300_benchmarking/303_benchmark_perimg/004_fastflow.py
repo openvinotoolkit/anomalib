86bf118f82751f6ec584ea3a2942bee947aff9f3
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2023-09-11 19:02:02 +0200

    add 004 fastflow

--- notebooks/300_benchmarking/303_benchmark_perimg/004_fastflow.py
+++ notebooks/300_benchmarking/303_benchmark_perimg/004_fastflow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# FastFlow
+# FastFlow WideResnet50
 
 # In[]:
 # Setup (pre-args)
@@ -14,7 +14,7 @@ import pandas as pd
 from IPython.core.interactiveshell import InteractiveShell
 
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-# os.environ['CUDA_VISIBLE_DEVICES'] = "2"  # DEBUG
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"  # DEBUG
 
 os.environ["WANDB_NOTEBOOK_NAME"] = __file__
 
@@ -65,6 +65,7 @@ from main import (  # noqa: E402
 # In[]:
 
 DEBUG = True
+DEBUG_PARAMS = False
 OFFLINE = True
 
 
@@ -93,8 +94,8 @@ seed_global = cliargs.seed_global
 seed_datamodule = cliargs.seed_datamodule
 
 # defaults
-train_batch_size = 4
-eval_batch_size = 4
+train_batch_size = 32
+eval_batch_size = 32
 num_workers = 8
 
 global_seeder = get_global_seeder(seed_global)
@@ -104,71 +105,81 @@ global_seeder = get_global_seeder(seed_global)
 # Model
 # This function is different in each script.
 
-from pytorch_lightning import Trainer  # noqa: E402
+from functools import partial, update_wrapper  # noqa: E402
+from types import MethodType  # noqa: E402
 
-from functools import partial, update_wrapper
-from types import MethodType
+from pytorch_lightning import LightningModule, Trainer  # noqa: E402
+from torch.optim import Adam, Optimizer  # noqa: E402
 
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from pytorch_lightning import LightningModule, Trainer
-from torch.optim import Optimizer, Adam
-
-from anomalib.data import TaskType
-from anomalib.models import Fastflow
-
-from anomalib.models import Patchcore  # noqa: E402
+from anomalib.models import Fastflow  # noqa: E402
 
 if DEBUG:
     MODELNAME = "debug"
 else:
-    MODELNAME = "fastflow"
+    MODELNAME = "fastflow_wr50"
 
 
 def get_model_trainer(logger=None):
-# based on `anomalib/notebooks/200_models/201_fastflow.ipynb`
-# https://github.com/openvinotoolkit/anomalib/blob/52bbb0b2417461ff097a819a06cc7ac3ff408149/notebooks/200_models/201_fastflow.ipynb
+    # based on `anomalib/notebooks/200_models/201_fastflow.ipynb`
+    # https://github.com/openvinotoolkit/anomalib/blob/52bbb0b2417461ff097a819a06cc7ac3ff408149/notebooks/200_models/201_fastflow.ipynb
 
-%autoreload 2
-
-if model_name == "fastflow":
-
-
-    if model_version != "default":
-        raise ValueError(f"Unknown model version: `{model_version}` for `{model_name}`") 
-    
     def configure_optimizers(lightning_module: LightningModule, optimizer: Optimizer):
         """Override to customize the LightningModule.configure_optimizers` method."""
         return optimizer
 
+    model_params = (
+        # DEBUG
+        dict(
+            backbone="wide_resnet50_2",
+            flow_steps=1,
+            conv3x3_only=False,
+            hidden_ratio=0.1,
+        )
+        if DEBUG and DEBUG_PARAMS
+        else
+        # PROD
+        dict(
+            backbone="wide_resnet50_2",
+            flow_steps=8,
+            conv3x3_only=False,  # OK
+            hidden_ratio=1.0,  # OK
+        )
+    )
+
     model = Fastflow(
-        input_size=(INPUT_IMAGE_RESOLUTION, INPUT_IMAGE_RESOLUTION), 
-        # defaults from
-        # https://github.com/openvinotoolkit/anomalib/blob/52bbb0b2417461ff097a819a06cc7ac3ff408149/src/anomalib/models/fastflow/config.yaml
-        backbone="resnet18", 
+        input_size=(INPUT_IMAGE_RESOLUTION, INPUT_IMAGE_RESOLUTION),
         pre_trained=True,
-        flow_steps=8,
-        hidden_ratio=1.0,
-        conv3x3_only=True,
+        **model_params,
     )
 
     fn = partial(
-        configure_optimizers, 
-        optimizer=Adam(params=model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=1e-5)
+        configure_optimizers, optimizer=Adam(params=model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=1e-5)
     )
     update_wrapper(fn, configure_optimizers)  # necessary for `is_overridden`
     model.configure_optimizers = MethodType(fn, model)
 
+    trainer_params = (
+        # DEBUG
+        dict(
+            accelerator="cpu",
+            devices=1,
+            max_epochs=1,
+        )
+        if DEBUG and DEBUG_PARAMS
+        else
+        # PROD
+        dict(
+            accelerator="cuda",
+            devices=1,
+            max_epochs=500,  # OK
+        )
+    )
+
     trainer = Trainer(
+        enable_progress_bar=True,  # TODO change me!
         logger=logger,
-        callbacks=[
-            METRICS_CALLBACK, POSTPROCESSING_CALLBACK,
-            ModelCheckpoint(mode="max", monitor="pixel_AUROC",),
-            EarlyStopping(monitor="pixel_AUROC", mode="max", patience=3,),
-        ],
-        accelerator="auto",  
-        devices=1,
-        max_epochs=1,  # DEBUG  
-        # max_epochs=100,  # PROD
+        callbacks=STANDARD_CALLBACKS,
+        **trainer_params,
     )
     return model, trainer
 
