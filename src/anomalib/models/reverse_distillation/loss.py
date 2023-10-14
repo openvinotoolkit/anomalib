e85de731c8dae3325234ebcef7f474b048ea6455
abc-125 <63813435+abc-125@users.noreply.github.com>
2023-10-14 16:29:55 +0200

    Refactor Reverse Distillation to match official code (#1389)
    * Non-mandatory early stopping
    
    * Added conv4 and bn4 to OCBE
    
    * Loss as in the official code (flattened arrays)
    
    * Added comment on how to use torchvision model as an encoder to reproduce results in the paper
    
    * Remove early stop from config, change default anomaly_map_mode to add
    
    * pre-commit fix
    
    * Updated results
    
    * Update src/anomalib/models/reverse_distillation/README.md
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    
    * Update src/anomalib/models/reverse_distillation/README.md
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    
    * Update src/anomalib/models/reverse_distillation/README.md
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    
    * Remove early_stopping
    
    * Update src/anomalib/models/reverse_distillation/lightning_model.py
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    
    * Easier to read code
    
    ---------
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>

--- src/anomalib/models/reverse_distillation/loss.py
+++ src/anomalib/models/reverse_distillation/loss.py
@@ -15,6 +15,10 @@ class ReverseDistillationLoss(nn.Module):
     def forward(self, encoder_features: list[Tensor], decoder_features: list[Tensor]) -> Tensor:
         """Computes cosine similarity loss based on features from encoder and decoder.
 
+        Based on the official code:
+        https://github.com/hq-deng/RD4AD/blob/6554076872c65f8784f6ece8cfb39ce77e1aee12/main.py#L33C25-L33C25
+        Calculates loss from flattened arrays of features, see https://github.com/hq-deng/RD4AD/issues/22
+
         Args:
             encoder_features (list[Tensor]): List of features extracted from encoder
             decoder_features (list[Tensor]): List of features extracted from decoder
@@ -23,8 +27,13 @@ class ReverseDistillationLoss(nn.Module):
             Tensor: Cosine similarity loss
         """
         cos_loss = torch.nn.CosineSimilarity()
-        losses = list(map(cos_loss, encoder_features, decoder_features))
         loss_sum = 0
-        for loss in losses:
-            loss_sum += torch.mean(1 - loss)  # mean of cosine distance
+        for encoder_feature, decoder_feature in zip(encoder_features, decoder_features):
+            loss_sum += torch.mean(
+                1
+                - cos_loss(
+                    encoder_feature.view(encoder_feature.shape[0], -1),
+                    decoder_feature.view(decoder_feature.shape[0], -1),
+                )
+            )
         return loss_sum
