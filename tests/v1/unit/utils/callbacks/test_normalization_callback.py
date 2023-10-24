efaca4f45796c35efb558d0b2baddc27da3a2c9e
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2023-10-24 16:06:20 +0200

    use mvtec dataset

--- tests/v1/unit/utils/callbacks/test_normalization_callback.py
+++ tests/v1/unit/utils/callbacks/test_normalization_callback.py
@@ -9,12 +9,12 @@ from lightning.pytorch import seed_everything
 from anomalib.data import MVTec
 from anomalib.engine import Engine
 from anomalib.models import Padim
-from tests.helpers.dataset import TestDataset, get_dataset_path
+from tests.helpers.dataset import get_dataset_path
 
 
-def run_train_test(normalization_method: str, root: str, category: str):
+def run_train_test(normalization_method: str):
     model = Padim()
-    datamodule = MVTec(root=root, category=category)
+    datamodule = MVTec(root=get_dataset_path(), category="bottle", seed=42)
 
     engine = Engine(
         normalization=normalization_method,
@@ -27,19 +27,18 @@ def run_train_test(normalization_method: str, root: str, category: str):
     return results
 
 
-@TestDataset(num_train=300, num_test=50, path=get_dataset_path(), seed=42)
-def test_normalizer(path=get_dataset_path(), category="shapes"):
+def test_normalizer():
     # run without normalization
     seed_everything(42)
-    results_without_normalization = run_train_test(normalization_method="none", root=path, category=category)
+    results_without_normalization = run_train_test(normalization_method="none")
 
     # run with cdf normalization
     seed_everything(42)
-    results_with_cdf_normalization = run_train_test(normalization_method="cdf", root=path, category=category)
+    results_with_cdf_normalization = run_train_test(normalization_method="cdf")
 
     # run without normalization
     seed_everything(42)
-    results_with_minmax_normalization = run_train_test(normalization_method="min_max", root=path, category=category)
+    results_with_minmax_normalization = run_train_test(normalization_method="min_max")
 
     # performance should be the same
     for metric in ["image_AUROC", "image_F1Score"]:
