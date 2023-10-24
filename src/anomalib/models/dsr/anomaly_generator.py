8a7f1d6399e16fd00414331a12c95e875fb5b56e
Philippe Carvalho <31983398+phcarval@users.noreply.github.com>
2023-10-24 12:14:19 +0200

    Add DSR model (#1142)
    * added license, init.py and draft readme
    
    * added draft DSR files
    
    * minor comment update
    
    * Implemented dsr model + comments
    
    * added dsr discrete model
    
    * added defect generation in torch model + dsr to list of existing methods in init.py
    
    * fixed torch model, started implementing lightning model, implemented anomaly generator
    
    * added loss file for DSR
    
    * Added loss, improved lightning module
    
    * Finished up global implementation of DSR second phase
    
    * minor fixes
    
    * Bugfixes
    
    * Fixed DSR loss calculation
    
    * on_training_start -> on_train_start
    
    * pre-commit run
    
    * updated DSR documentation
    
    * reset config file
    
    * added automatic pretraining weight download
    
    * testing pretrained weights. fixed embedding size in upsampling module and image recon module, to be fixed in original branch
    
    * successful testing on pretrained dsr weights
    
    * checked test quality with pretrained weights, fixed anomaly score calculation
    
    * training is functional
    
    * Fixed training procedure
    
    * test still working
    
    * working upsampling module training and testing
    
    * fixed minor bugs
    
    * updated documentation
    
    * added tests and doc
    
    * adapted learning schedule to steps
    
    * Update src/anomalib/models/dsr/anomaly_generator.py
    
    Co-authored-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    Co-authored-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * refactored outputs into dicts
    
    * remove super() args
    
    * changed downloading weights from anomalib releases + minor fixes
    
    * pre commit hooks + minor fixes
    
    * removed configurable ckpt path refs + default iteration nb from paper
    
    * cleaned up dsr.rst and turned exceptions into RuntimeErrors
    
    * Added upsampling ratio parameter to set third training phase epochs
    
    * Added batched evalaution + minor code simplification
    
    * pre commit hooks
    
    * squeeze output image score tensor
    
    * readded new path check in efficient ad
    
    * fixed double step count with manual optimization
    
    * fixed trailing whitespace
    
    * Fix black issues
    
    * Apply suggestions from code review
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    
    * review suggestions
    
    * updated architecture image links
    
    * Address mypy
    
    * changed output types for dsr model
    
    * readded dict outputs, adapted to TorchInferencer
    
    * fixed error in output dict
    
    * removed default imagenet norm
    
    ---------
    
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    Co-authored-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

---
+++ src/anomalib/models/dsr/anomaly_generator.py
@@ -0,0 +1,81 @@
+"""Anomaly generator for the DSR model implementation."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from typing import List
+
+import imgaug.augmenters as iaa
+import numpy as np
+import torch
+from torch import Tensor, nn
+
+from anomalib.data.utils.generators.perlin import _rand_perlin_2d_np
+
+
+class DsrAnomalyGenerator(nn.Module):
+    """Anomaly generator of the DSR model. The anomaly is generated using a Perlin
+    noise generator on the two quantized representations of an image. This generator
+    is only used during the second phase of training! The third phase requires generating
+    smudges over the input images.
+
+    Args:
+        p_anomalous (float, optional): Probability to generate an anomalous image.
+    """
+
+    def __init__(
+        self,
+        p_anomalous: float = 0.5,
+    ):
+        super().__init__()
+
+        self.p_anomalous = p_anomalous
+        self.rot = iaa.Sequential([iaa.Affine(rotate=(-90, 90))])
+
+    def generate_anomaly(self, height: int, width: int) -> Tensor:
+        """Generate an anomalous mask.
+
+        Args:
+            height (int): Height of generated mask.
+            width (int): Width of generated mask.
+
+        Returns:
+            Tensor: Generated mask.
+        """
+        min_perlin_scale = 0
+        perlin_scale = 6
+        perlin_scalex = 2 ** (torch.randint(min_perlin_scale, perlin_scale, (1,)).numpy()[0])
+        perlin_scaley = 2 ** (torch.randint(min_perlin_scale, perlin_scale, (1,)).numpy()[0])
+        threshold = 0.5
+        perlin_noise_np = _rand_perlin_2d_np((height, width), (perlin_scalex, perlin_scaley))
+        perlin_noise_np = self.rot(image=perlin_noise_np)
+        mask = np.where(perlin_noise_np > threshold, np.ones_like(perlin_noise_np), np.zeros_like(perlin_noise_np))
+        mask = np.expand_dims(mask, axis=2).astype(np.float32)
+        mask = torch.from_numpy(mask)
+
+        return mask
+
+    def augment_batch(self, batch: Tensor) -> Tensor:
+        """Generate anomalous augmentations for a batch of input images.
+
+        Args:
+            batch (Tensor): Batch of input images
+
+        Returns:
+            Tensor: Ground truth masks corresponding to the anomalous perturbations.
+        """
+        batch_size, _, height, width = batch.shape
+
+        # Collect perturbations
+        masks_list: List[Tensor] = []
+        for _ in range(batch_size):
+            if torch.rand(1) > self.p_anomalous:  # include normal samples
+                masks_list.append(torch.zeros((1, height, width)))
+            else:
+                mask = self.generate_anomaly(height, width)
+                masks_list.append(mask.permute((2, 0, 1)))
+
+        masks = torch.stack(masks_list).to(batch.device)
+
+        return masks
