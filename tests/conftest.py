795b7df9d729b70d3b22b7763015605c54374ac6
Samet <samet.akcay@intel.com>
2023-10-27 13:22:14 +0100

    Revert conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,20 @@
-"""Fixtures for anomalib test suite."""
+"""Fixtures for the entire test suite."""
 
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 
-from __future__ import annotations
-
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import TYPE_CHECKING, Any
 
 import pytest
 
 from tests.legacy.helpers.dataset import GeneratedDummyDataset
 
-if TYPE_CHECKING:
-    from collections.abc import Generator
-
 
 def _model_names() -> list[str]:
-    """Return list of model names.
+    """Return list of strings so that pytest can show the entire path of the yaml in the test log.
 
-    Return as strings so that pytest can show the entire path of the yaml in the test log.
     Path object is not serializable by pytest.
     """
     # TODO(ashwinvaidya17)
@@ -34,26 +27,23 @@ def _dataset_names() -> list[str]:
 
 
 @pytest.fixture(scope="session")
-def project_path() -> Generator[str, Any, None]:
-    """Fixture that returns a temporary project path."""
+def project_path():
     with TemporaryDirectory() as project_path:
         yield project_path
 
 
 @pytest.fixture(scope="session")
-def dataset_root() -> Generator[str, Any, None]:
+def dataset_root():
     """Generate a dummy dataset."""
     with GeneratedDummyDataset(num_train=20, num_test=10) as data_root:
         yield data_root
 
 
 @pytest.fixture(scope="session", params=_model_names())
-def model_name(request: type[pytest.FixtureRequest]) -> str:
-    """Fixture that returns a model name."""
+def model_name(request):
     return request.param
 
 
 @pytest.fixture(scope="session", params=_dataset_names())
-def dataset_name(request: type[pytest.FixtureRequest]) -> str:
-    """Fixture that returns a dataset name."""
+def dataset_name(request):
     return request.param
