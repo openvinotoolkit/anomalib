fee3e9ffd43dca69e560d49a7fcc56c8b8e4ee35
AlessandroPolidori <alessandro.polidori@orobix.com>
2023-10-27 08:43:29 +0000

    temp: Make efficient_ad deterministic

--- src/anomalib/models/efficient_ad/torch_model.py
+++ src/anomalib/models/efficient_ad/torch_model.py
@@ -242,7 +242,7 @@ def get_pdn_medium(out_channels=384, padding=False):
     )
 
 
-def get_autoencoder(out_channels=384):
+def get_autoencoder(out_channels: int = 384):
     return nn.Sequential(
         # encoder
         nn.Conv2d(in_channels=3, out_channels=32, kernel_size=4, stride=2, padding=1),
@@ -257,31 +257,31 @@ def get_autoencoder(out_channels=384):
         nn.ReLU(inplace=True),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=8),
         # decoder
-        nn.Upsample(size=3, mode="bilinear"),
+        nn.Upsample(size=3),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=8, mode="bilinear"),
+        nn.Upsample(size=8),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=15, mode="bilinear"),
+        nn.Upsample(size=15),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=32, mode="bilinear"),
+        nn.Upsample(size=32),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=63, mode="bilinear"),
+        nn.Upsample(size=63),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=127, mode="bilinear"),
+        nn.Upsample(size=127),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=56, mode="bilinear"),
+        nn.Upsample(size=56),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
         nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
@@ -426,15 +426,15 @@ class EfficientAdModel(nn.Module):
 
             student_output_ae_aug = self.student(aug_img)[:, self.teacher_out_channels :, :, :]
 
-            if teacher_output_aug.shape != ae_output_aug.shape:
-                teacher_output_aug = F.interpolate(teacher_output_aug, size=ae_output_aug.shape[2:], mode="bilinear")
+            # if teacher_output_aug.shape != ae_output_aug.shape:
+            #    teacher_output_aug = F.interpolate(teacher_output_aug, size=ae_output_aug.shape[2:], mode="bilinear")
 
             distance_ae = torch.pow(teacher_output_aug - ae_output_aug, 2)
 
-            if student_output_ae_aug.shape != ae_output_aug.shape:
-                student_output_ae_aug = F.interpolate(
-                    student_output_ae_aug, size=ae_output_aug.shape[2:], mode="bilinear"
-                )
+            # if student_output_ae_aug.shape != ae_output_aug.shape:
+            #    student_output_ae_aug = F.interpolate(
+            #        student_output_ae_aug, size=ae_output_aug.shape[2:], mode="bilinear"
+            #    )
 
             distance_stae = torch.pow(ae_output_aug - student_output_ae_aug, 2)
 
@@ -448,8 +448,8 @@ class EfficientAdModel(nn.Module):
 
             map_st = torch.mean(distance_st, dim=1, keepdim=True)
 
-            if student_output.shape != ae_output.shape:
-                student_output = F.interpolate(student_output, size=ae_output.shape[2:], mode="bilinear")
+            # if student_output.shape != ae_output.shape:
+            #    student_output = F.interpolate(student_output, size=ae_output.shape[2:], mode="bilinear")
 
             map_stae = torch.mean(
                 (ae_output - student_output[:, self.teacher_out_channels :]) ** 2, dim=1, keepdim=True
