a665aa5f7c1c7af7b5282e560cc2843df58f976a
Ashwin Vaidya <ashwin.vaidya@intel.com>
2023-11-01 16:32:46 +0100

    Add ruff checks to tests (#1455)
    * Fix tests + add ruff check to tests
    
    * Limit gradio version
    
    * Path->str
    
    ---------
    
    Co-authored-by: Ashwin Vaidya <ashwinitinvaidya@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from collections.abc import Generator
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -17,8 +18,8 @@ def _model_names() -> list[str]:
 
     Path object is not serializable by pytest.
     """
-    # TODO(ashwinvaidya17)
-    # Restore testing of ai_vad model.
+    # TODO(ashwinvaidya17): Restore testing of ai_vad model.
+    # CVS-124134
     return [str(path.stem) for path in Path("src/configs/model").glob("*.yaml") if path.stem != "ai_vad"]
 
 
@@ -27,23 +28,26 @@ def _dataset_names() -> list[str]:
 
 
 @pytest.fixture(scope="session")
-def project_path():
+def project_path() -> Generator[Path, None, None]:
+    """Return a temporary directory path that is used as the project directory for the entire test."""
     with TemporaryDirectory() as project_path:
-        yield project_path
+        yield Path(project_path)
 
 
 @pytest.fixture(scope="session")
-def dataset_root():
+def dataset_root() -> Generator[Path, None, None]:
     """Generate a dummy dataset."""
     with GeneratedDummyDataset(num_train=20, num_test=10) as data_root:
-        yield data_root
+        yield Path(data_root)
 
 
 @pytest.fixture(scope="session", params=_model_names())
-def model_name(request):
+def model_name(request: "pytest.FixtureRequest") -> list[str]:
+    """Return the list of names of all the models."""
     return request.param
 
 
 @pytest.fixture(scope="session", params=_dataset_names())
-def dataset_name(request):
+def dataset_name(request: "pytest.FixtureRequest") -> list[str]:
+    """Return the list of names of all the datasets."""
     return request.param
--- tests/integration/cli/test_cli.py
+++ tests/integration/cli/test_cli.py
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from pathlib import Path
+
+import pytest
 import torch
 from tests.legacy.helpers.dataset import get_dataset_path
 
@@ -13,7 +16,7 @@ from anomalib.utils.cli import AnomalibCLI
 class TestCLI:
     """Do sanity check on all models."""
 
-    def test_fit(self, model_name: str, dataset_root: str, project_path: str):
+    def test_fit(self, model_name: str, dataset_root: Path, project_path: Path) -> None:
         """Test fit CLI.
 
         Args:
@@ -24,8 +27,8 @@ class TestCLI:
         data_class = "anomalib.data.MVTec"
         category = "shapes"
         if model_name == "ai_vad":
-            # TODO(ashwinva)
-            # fix this when the dummy dataset supports all the data formats
+            # TODO(ashwinva): use dummy dataset path when it supports all the data formats
+            # CVS-109972
             # aivad expects UCSD dataset
             data_class = "anomalib.data.UCSDped"
             dataset_root = get_dataset_path(dataset="ucsd")
@@ -40,13 +43,13 @@ class TestCLI:
                 "--data",
                 data_class,
                 "--data.root",
-                dataset_root,
+                str(dataset_root),
                 "--data.category",
                 category,
                 "--data.train_batch_size",
                 "8",
                 "--results_dir.path",
-                project_path,
+                str(project_path),
                 "--results_dir.unique",
                 "false",
                 "--trainer.max_epochs",
@@ -64,16 +67,25 @@ class TestCLI:
                 "true",
                 "--trainer.callbacks.auto_insert_metric_name",
                 "false",
-            ]
+            ],
         )
         torch.cuda.empty_cache()
 
-    def test_test(self, model_name: str, dataset_root: str, project_path: str):
+    def test_test(self, model_name: str, dataset_root: Path, project_path: Path) -> None:
+        """Test the test method of the CLI.
+
+        Args:
+            model_name (str): Name of the model to train.
+            dataset_root (str): Root of the synthetic/original dataset.
+            project_path (str): Path to temporary project folder.
+        """
         data_class = "anomalib.data.MVTec"
         category = "shapes"
+        dataset_root = str(dataset_root)
+        project_path = str(project_path)
         if model_name == "ai_vad":
-            # TODO(ashwinva)
-            # fix this when the dummy dataset supports all the data formats
+            # TODO(ashwinva): use dummy dataset path when it supports all the data formats
+            # CVS-109972
             # aivad expects UCSD dataset
             data_class = "anomalib.data.UCSDped"
             dataset_root = get_dataset_path(dataset="ucsd")
@@ -87,51 +99,61 @@ class TestCLI:
                 "--data",
                 data_class,
                 "--data.root",
-                dataset_root,
+                str(dataset_root),
                 "--data.category",
                 category,
                 "--results_dir.path",
-                project_path,
+                str(project_path),
                 "--results_dir.unique",
                 "false",
                 "--ckpt_path",
                 f"{project_path}/{model_name}/{data_class.split('.')[-1].lower()}/{category}/weights/last.ckpt",
-            ]
+            ],
         )
         torch.cuda.empty_cache()
 
-    # def test_validate(self, model_name: str, dataset_root: str, project_path: str):
-    #     data_class = "anomalib.data.MVTec"
-    #     category = "shapes"
-    #     if model_name == "ai_vad":
-    #         # TODO(ashwinva)
-    #         # fix this when the dummy dataset supports all the data formats
-    #         # aivad expects UCSD dataset
-    #         data_class = "anomalib.data.UCSDped"
-    #         dataset_root = get_dataset_path(dataset="ucsd")
-    #         category = "UCSDped2"
+    @pytest.mark.skip(reason="validation is not implemented in Anomalib Engine")
+    def test_validate(self, model_name: str, dataset_root: Path, project_path: Path) -> None:
+        """Test the validate method of the CLI.
 
-    #     AnomalibCLI(
-    #         args=[
-    #             "validate",
-    #             "-c",
-    #             f"src/configs/model/{model_name}.yaml",
-    #             "--data",
-    #             data_class,
-    #             "--data.root",
-    #             dataset_root,
-    #             "--data.category",
-    #             category,
-    #             "--results_dir.path",
-    #             project_path,
-    #             "--results_dir.unique",
-    #             "false",
-    #             "--ckpt_path",
-    #             f"{project_path}/{model_name}/{data_class.split('.')[-1].lower()}/{category}/weights/last.ckpt",
-    #         ]
-    #     )
-    #     torch.cuda.empty_cache()
+        Args:
+            model_name (str): Name of the model to train.
+            dataset_root (str): Root of the synthetic/original dataset.
+            project_path (str): Path to temporary project folder.
+        """
+        data_class = "anomalib.data.MVTec"
+        category = "shapes"
+        if model_name == "ai_vad":
+            # TODO(ashwinva): use dummy dataset path when it supports all the data formats
+            # CVS-109972
+            # aivad expects UCSD dataset
+            data_class = "anomalib.data.UCSDped"
+            dataset_root = get_dataset_path(dataset="ucsd")
+            category = "UCSDped2"
 
-    # TODO(ashwinva) Predict
+        AnomalibCLI(
+            args=[
+                "validate",
+                "-c",
+                f"src/configs/model/{model_name}.yaml",
+                "--data",
+                data_class,
+                "--data.root",
+                str(dataset_root),
+                "--data.category",
+                category,
+                "--results_dir.path",
+                str(project_path),
+                "--results_dir.unique",
+                "false",
+                "--ckpt_path",
+                f"{project_path}/{model_name}/{data_class.split('.')[-1].lower()}/{category}/weights/last.ckpt",
+            ],
+        )
+        torch.cuda.empty_cache()
 
-    # TODO(ashwinva) export
+    # TODO(ashwinvaidya17): Predict
+    # CVS-109972
+
+    # TODO(ashwinvaidya17): export
+    # CVS-109972
