a525c4776b9979e441a3193263103ba1a736115b
Samet <samet.akcay@intel.com>
2023-11-15 06:35:24 +0000

    torch_all to torch.all

--- tests/legacy/pre_merge/utils/metrics/test_binning.py
+++ tests/legacy/pre_merge/utils/metrics/test_binning.py
@@ -6,9 +6,9 @@ from anomalib.utils.metrics.binning import thresholds_between_min_and_max, thres
 
 def test_thresholds_between_min_and_max():
     preds = torch.Tensor([1, 10])
-    assert torch_all(thresholds_between_min_and_max(preds, 2) == preds)
+    assert torch.all(thresholds_between_min_and_max(preds, 2) == preds)
 
 
 def test_thresholds_between_0_and_1():
     expected = torch.Tensor([0, 1])
-    assert torch_all(thresholds_between_0_and_1(2) == expected)
+    assert torch.all(thresholds_between_0_and_1(2) == expected)
