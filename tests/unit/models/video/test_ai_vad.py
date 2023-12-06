566c5cff760a2cc2b5aebefadf0cf5c34f31be96
Dick Ameln <dick.ameln@intel.com>
2023-12-06 18:18:06 +0100

    add unit tests for feature extractors

---
+++ tests/unit/models/video/test_ai_vad.py
@@ -0,0 +1,53 @@
+"""Unit tests for AI-VAD video anomaly detection model."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+
+from anomalib.models.video.ai_vad import AiVad
+
+
+class TestAiVadFeatureExtractor:
+    """Test if the different feature extractors of the AiVad model can handle edge case without bbox detections."""
+
+    def test_velocity_extractor(self) -> None:
+        """Test velocity extractor submodule."""
+        pl_module = AiVad()
+        velocity_feature_extractor = pl_module.model.feature_extractor.velocity_extractor
+
+        flow_data = torch.zeros(4, 2, 256, 256)
+        boxes = torch.empty(0, 5)
+
+        velocity_features = velocity_feature_extractor(flow_data, boxes)
+
+        # features should be empty because there are no boxes
+        assert velocity_features.numel() == 0
+
+    def test_deep_feature_extractor(self) -> None:
+        """Test deep feature extractor submodule."""
+        pl_module = AiVad()
+        deep_feature_extractor = pl_module.model.feature_extractor.deep_extractor
+
+        batch_size = 4
+        rgb_data = torch.zeros(batch_size, 3, 256, 256)
+        boxes = torch.empty(0, 5)
+
+        deep_features = deep_feature_extractor(rgb_data, boxes, batch_size)
+
+        # features should be empty because there are no boxes
+        assert deep_features.numel() == 0
+
+    def test_pose_feature_extractor(self) -> None:
+        """Test pose feature extractor submodule."""
+        pl_module = AiVad()
+        pose_feature_extractor = pl_module.model.feature_extractor.pose_extractor
+
+        batch_size = 4
+        rgb_data = torch.zeros(batch_size, 3, 256, 256)
+        boxes = [torch.empty(0, 4)] * batch_size
+
+        pose_features = pose_feature_extractor(rgb_data, boxes)
+
+        # features should be empty because there are no boxes
+        assert torch.vstack(pose_features).numel() == 0
