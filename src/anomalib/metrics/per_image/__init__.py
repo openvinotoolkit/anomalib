4bfe3da330d4061ef978e03e56fcb849f9cc2c7d
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2023-12-23 19:44:18 +0100

    renames and put things in init
    Signed-off-by: jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>

--- src/anomalib/metrics/per_image/__init__.py
+++ src/anomalib/metrics/per_image/__init__.py
@@ -1,1 +1,26 @@
 """Per-Image Metrics."""
+
+from .binclf_curve import per_image_binclf_curve, per_image_fpr, per_image_tpr
+from .binclf_curve_numpy import Algorithm as BinClfAlgorithm
+from .binclf_curve_numpy import ThreshsChoice as BinclfThreshsChoice
+from .pimo import AUPIMO, PIMO, AUPIMOResult, PIMOResult, aupimo_scores, pimo_curves
+from .pimo_numpy import SharedFPRMetric
+
+__all__ = [
+    # constants
+    "BinClfAlgorithm",
+    "BinclfThreshsChoice",
+    "SharedFPRMetric",
+    # result classes
+    "PIMOResult",
+    "AUPIMOResult",
+    # functional interfaces
+    "per_image_binclf_curve",
+    "per_image_fpr",
+    "per_image_tpr",
+    "pimo_curves",
+    "aupimo_scores",
+    # torchmetrics interfaces
+    "PIMO",
+    "AUPIMO",
+]
