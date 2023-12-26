408fb2bdd45b79424fb2a942b53b93af02475d15
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2023-12-26 12:25:36 +0100

    refactor constants notation
    Signed-off-by: jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>

--- src/anomalib/metrics/per_image/__init__.py
+++ src/anomalib/metrics/per_image/__init__.py
@@ -4,20 +4,19 @@ TODO(jpcbertoldo): add formalities (license header, author)
 """
 
 from .binclf_curve import per_image_binclf_curve, per_image_fpr, per_image_tpr
-from .binclf_curve_numpy import Algorithm as BinClfAlgorithm
-from .binclf_curve_numpy import ThreshsChoice as BinclfThreshsChoice
+from .binclf_curve_numpy import BinclfAlgorithm, BinclfThreshsChoice
 from .pimo import AUPIMO, PIMO, AUPIMOResult, PIMOResult, aupimo_scores, pimo_curves
-from .pimo_numpy import SharedFPRMetric
+from .pimo_numpy import PIMOSharedFPRMetric
 from .utils import per_image_scores_stats
 from .utils_numpy import StatsOutliersPolicy, StatsRepeatedPolicy
 
 __all__ = [
     # constants
-    "BinClfAlgorithm",
+    "BinclfAlgorithm",
     "BinclfThreshsChoice",
     "StatsOutliersPolicy",
     "StatsRepeatedPolicy",
-    "SharedFPRMetric",
+    "PIMOSharedFPRMetric",
     # result classes
     "PIMOResult",
     "AUPIMOResult",
