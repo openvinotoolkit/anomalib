08657656a4b5b877a57f14c47884356dbb37526e
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-01-22 00:51:31 +0900

    Add sPro metric implementation

--- src/anomalib/metrics/__init__.py
+++ src/anomalib/metrics/__init__.py
@@ -19,6 +19,7 @@ from .auroc import AUROC
 from .collection import AnomalibMetricCollection
 from .min_max import MinMax
 from .pro import PRO
+from .spro import sPRO
 from .threshold import F1AdaptiveThreshold, ManualThreshold
 
 __all__ = [
@@ -30,6 +31,7 @@ __all__ = [
     "ManualThreshold",
     "MinMax",
     "PRO",
+    "sPRO",
 ]
 
 logger = logging.getLogger(__name__)
