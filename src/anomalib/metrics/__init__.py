451887009bfa45081ce97b6d1c229d98cd63c378
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-02-01 07:57:39 +0900

    rename spro metric and change the default value of saturation_config to None
    Signed-off-by: Willy Fitra Hendria <willyfitrahendria@gmail.com>

--- src/anomalib/metrics/__init__.py
+++ src/anomalib/metrics/__init__.py
@@ -19,7 +19,7 @@ from .auroc import AUROC
 from .collection import AnomalibMetricCollection
 from .min_max import MinMax
 from .pro import PRO
-from .spro import sPRO
+from .spro import SPRO
 from .threshold import F1AdaptiveThreshold, ManualThreshold
 
 __all__ = [
@@ -31,7 +31,7 @@ __all__ = [
     "ManualThreshold",
     "MinMax",
     "PRO",
-    "sPRO",
+    "SPRO",
 ]
 
 logger = logging.getLogger(__name__)
