810ff03e37dc0c0dffbf965e632aba9f1027495d
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2024-02-27 15:58:27 +0100

    modify args for other entrypoints
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/anomalib/engine/engine.py
+++ src/anomalib/engine/engine.py
@@ -308,7 +308,7 @@ class Engine:
     def _should_run_validation(
         self,
         model: AnomalyModule,
-        dataloaders: EVAL_DATALOADERS | AnomalibDataModule | None,
+        dataloaders: EVAL_DATALOADERS | None,
         datamodule: AnomalibDataModule | None,
         ckpt_path: str | None,
     ) -> bool:
@@ -326,7 +326,7 @@ class Engine:
 
         Args:
             model (AnomalyModule): Model passed to the entrypoint.
-            dataloaders (EVAL_DATALOADERS | AnomalibDataModule | None): Dataloaders passed to the entrypoint.
+            dataloaders (EVAL_DATALOADERS | None): Dataloaders passed to the entrypoint.
             datamodule (AnomalibDataModule | None): Lightning datamodule passed to the entrypoint.
             ckpt_path (str | None): Checkpoint path passed to the entrypoint.
 
@@ -348,7 +348,7 @@ class Engine:
     def fit(
         self,
         model: AnomalyModule,
-        train_dataloaders: TRAIN_DATALOADERS | AnomalibDataModule | None = None,
+        train_dataloaders: TRAIN_DATALOADERS | None = None,
         val_dataloaders: EVAL_DATALOADERS | None = None,
         datamodule: AnomalibDataModule | None = None,
         ckpt_path: str | None = None,
@@ -357,7 +357,7 @@ class Engine:
 
         Args:
             model (AnomalyModule): Model to be trained.
-            train_dataloaders (TRAIN_DATALOADERS | AnomalibDataModule | None, optional): Train dataloaders.
+            train_dataloaders (TRAIN_DATALOADERS | None, optional): Train dataloaders.
                 Defaults to None.
             val_dataloaders (EVAL_DATALOADERS | None, optional): Validation dataloaders.
                 Defaults to None.
@@ -392,7 +392,7 @@ class Engine:
     def validate(
         self,
         model: AnomalyModule | None = None,
-        dataloaders: EVAL_DATALOADERS | AnomalibDataModule | None = None,
+        dataloaders: EVAL_DATALOADERS | None = None,
         ckpt_path: str | None = None,
         verbose: bool = True,
         datamodule: AnomalibDataModule | None = None,
@@ -402,7 +402,7 @@ class Engine:
         Args:
             model (AnomalyModule | None, optional): Model to be validated.
                 Defaults to None.
-            dataloaders (EVAL_DATALOADERS | AnomalibDataModule | None, optional): Dataloaders to be used for
+            dataloaders (EVAL_DATALOADERS | None, optional): Dataloaders to be used for
                 validation.
                 Defaults to None.
             ckpt_path (str | None, optional): Checkpoint path. If provided, the model will be loaded from this path.
@@ -439,7 +439,7 @@ class Engine:
     def test(
         self,
         model: AnomalyModule | None = None,
-        dataloaders: EVAL_DATALOADERS | AnomalibDataModule | None = None,
+        dataloaders: EVAL_DATALOADERS | None = None,
         ckpt_path: str | None = None,
         verbose: bool = True,
         datamodule: AnomalibDataModule | None = None,
@@ -453,7 +453,7 @@ class Engine:
             model (AnomalyModule | None, optional):
                 The model to be tested.
                 Defaults to None.
-            dataloaders (EVAL_DATALOADERS | AnomalibDataModule | None, optional):
+            dataloaders (EVAL_DATALOADERS | None, optional):
                 An iterable or collection of iterables specifying test samples.
                 Defaults to None.
             ckpt_path (str | None, optional):
@@ -526,8 +526,6 @@ class Engine:
             self.trainer.validate(model, dataloaders, None, verbose=False, datamodule=datamodule)
         return self.trainer.test(model, dataloaders, ckpt_path, verbose, datamodule)
 
-    # TODO(ashwinvaidya17): revisit typing of data args
-    # https://github.com/openvinotoolkit/anomalib/issues/1638
     def predict(
         self,
         model: AnomalyModule | None = None,
@@ -631,7 +629,7 @@ class Engine:
     def train(
         self,
         model: AnomalyModule,
-        train_dataloaders: TRAIN_DATALOADERS | AnomalibDataModule | None = None,
+        train_dataloaders: TRAIN_DATALOADERS | None = None,
         val_dataloaders: EVAL_DATALOADERS | None = None,
         test_dataloaders: EVAL_DATALOADERS | None = None,
         datamodule: AnomalibDataModule | None = None,
@@ -641,7 +639,7 @@ class Engine:
 
         Args:
             model (AnomalyModule): Model to be trained.
-            train_dataloaders (TRAIN_DATALOADERS | AnomalibDataModule | None, optional): Train dataloaders.
+            train_dataloaders (TRAIN_DATALOADERS | None, optional): Train dataloaders.
                 Defaults to None.
             val_dataloaders (EVAL_DATALOADERS | None, optional): Validation dataloaders.
                 Defaults to None.
