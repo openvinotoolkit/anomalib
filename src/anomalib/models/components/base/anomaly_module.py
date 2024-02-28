9d42b496d56d835b37ff4ebd6425809e6c9399c9
Dick Ameln <dick.ameln@intel.com>
2024-02-28 23:33:48 +0100

    enable antialias in default transform

--- src/anomalib/models/components/base/anomaly_module.py
+++ src/anomalib/models/components/base/anomaly_module.py
@@ -225,7 +225,7 @@ class AnomalyModule(pl.LightningModule, ABC):
         image_size = image_size or (256, 256)
         return Compose(
             [
-                Resize(image_size),
+                Resize(image_size, antialias=True),
                 Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ],
         )
