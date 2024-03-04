becdcc8df66b9b5352c748dce92729e8f1952f93
sahusiddharth <siddharth.sahu@plaksha.edu.in>
2024-03-04 16:24:40 +0530

    Signed
    Signed-off-by: sahusiddharth <siddharth.sahu@plaksha.edu.in>

--- src/anomalib/data/errors.py
+++ src/anomalib/data/errors.py
@@ -1,2 +1,13 @@
 class MisMatchError(Exception):
-    pass
+    """Exception raised when a mismatch is detected.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
+
+    def __init__(self, message=""):
+        if message:
+            self.message = message
+        else:
+            self.message = "Mismatch detected."
+        super().__init__(self.message)
