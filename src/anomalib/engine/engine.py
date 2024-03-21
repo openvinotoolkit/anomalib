686cf84196849ddb96854034fd15bbd77535266f
Ashwin Vaidya <ashwin.vaidya@intel.com>
2024-03-21 18:11:05 +0100

    ✨ Upgrade TorchMetrics (#1879)
    * 🚧 Temporarily enable tests on hotfix branches (#1840)
    
    * temporarily enable tests on hotfix branches
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * temporarily enable tests on hotfix branches
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    ---------
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * 🔨 Migrate to custom precision recall curve (#1829)
    
    * Migrate to custom precision recall curve
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * upgrade torchmetrics
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Remove check
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Fix CLI
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Fix PRO computation
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Fix typo
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Skip normalization tests
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    ---------
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Update imports + remove hotfix branch testing from CI
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Use static method
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    * Add warnings
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
    
    ---------
    
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/anomalib/engine/engine.py
+++ src/anomalib/engine/engine.py
@@ -109,10 +109,10 @@ class Engine:
         threshold (THRESHOLD):
             Thresholding method. Defaults to "F1AdaptiveThreshold".
         task (TaskType, optional): Task type. Defaults to TaskType.SEGMENTATION.
-        image_metrics (str | list[str] | None, optional): Image metrics to be used for evaluation.
-            Defaults to None.
-        pixel_metrics (str | list[str] | None, optional): Pixel metrics to be used for evaluation.
-            Defaults to None.
+        image_metrics (list[str] | str | dict[str, dict[str, Any]] | None, optional): Image metrics to be used for
+            evaluation. Defaults to None.
+        pixel_metrics (list[str] | str | dict[str, dict[str, Any]] | None, optional): Pixel metrics to be used for
+            evaluation. Defaults to None.
         default_root_dir (str, optional): Default root directory for the trainer.
             The results will be saved in this directory.
             Defaults to ``results``.
@@ -125,8 +125,8 @@ class Engine:
         normalization: NORMALIZATION = NormalizationMethod.MIN_MAX,
         threshold: THRESHOLD = "F1AdaptiveThreshold",
         task: TaskType | str = TaskType.SEGMENTATION,
-        image_metrics: str | list[str] | None = None,
-        pixel_metrics: str | list[str] | None = None,
+        image_metrics: list[str] | str | dict[str, dict[str, Any]] | None = None,
+        pixel_metrics: list[str] | str | dict[str, dict[str, Any]] | None = None,
         logger: Logger | Iterable[Logger] | bool | None = None,
         default_root_dir: str | Path = "results",
         **kwargs,
