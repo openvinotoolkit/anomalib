55700c2e4acd7f05d9c7e2fd44b7de10d67d1d52
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2024-03-21 16:52:08 +0100

    Add warnings
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/anomalib/metrics/f1_score.py
+++ src/anomalib/metrics/f1_score.py
@@ -7,7 +7,12 @@ This is added for convenience.
 # SPDX-License-Identifier: Apache-2.0
 
 
+import logging
+from typing import Any, Literal
+
 from torchmetrics.classification import BinaryF1Score
+
+logger = logging.getLogger(__name__)
 
 
 class F1Score(BinaryF1Score):
@@ -16,3 +21,17 @@ class F1Score(BinaryF1Score):
     The idea behind this is to retain the current configuration otherwise the one from
     torchmetrics requires ``task`` as a parameter.
     """
+
+    def __init__(
+        self,
+        threshold: float = 0.5,
+        multidim_average: Literal["global"] | Literal["samplewise"] = "global",
+        ignore_index: int | None = None,
+        validate_args: bool = True,
+        **kwargs: Any,  # noqa: ANN401
+    ) -> None:
+        super().__init__(threshold, multidim_average, ignore_index, validate_args, **kwargs)
+        logger.warning(
+            "F1Score class exists for backwards compatibility. It will be removed in v1.1."
+            " Please use BinaryF1Score from torchmetrics instead",
+        )
