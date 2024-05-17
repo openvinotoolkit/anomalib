131fa892e95b9c8de56580eefbfcbefb2683d8ac
Nilesh Ahuja <nilesh.ahuja@intel.com>
2024-05-16 23:19:08 -0700

    Fixing max_epochs to 220

--- src/anomalib/models/image/fre/lightning_model.py
+++ src/anomalib/models/image/fre/lightning_model.py
@@ -109,7 +109,7 @@ class Fre(AnomalyModule):
     @property
     def trainer_arguments(self) -> dict[str, Any]:
         """Return FRE-specific trainer arguments."""
-        return {"gradient_clip_val": 0, "max_epochs": 22, "num_sanity_val_steps": 0}
+        return {"gradient_clip_val": 0, "max_epochs": 220, "num_sanity_val_steps": 0}
 
     @property
     def learning_type(self) -> LearningType:
