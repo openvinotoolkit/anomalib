ccda74dd8b6550c78b550526fb398f6a218acc59
slutzky <slutzky@aligntech.com>
2024-07-04 10:11:54 +0300

    remove torch install args

--- src/anomalib/cli/install.py
+++ src/anomalib/cli/install.py
@@ -62,6 +62,7 @@ def anomalib_install(option: str = "full", verbose: bool = False) -> int:
     if option in ("full", "core") and torch_requirement is not None:
         torch_install_args = get_torch_install_args(torch_requirement)
 
+
     # Combine torch and other requirements.
     # install_args = other_requirements + torch_install_args
     install_args = other_requirements
