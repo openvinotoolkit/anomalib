029e262ca67f126e193a890210f54152268952a0
Samet <samet.akcay@intel.com>
2024-07-05 15:50:26 +0100

    Fix requirement parsing issue, and pre-commit

--- src/anomalib/data/utils/boxes.py
+++ src/anomalib/data/utils/boxes.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2022-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-
 import torch
 
 from anomalib.utils.cv import connected_components_cpu, connected_components_gpu
