afaec9b8c55ad9e7dfbc9e64aa958185b60a25bb
Dick Ameln <dick.ameln@intel.com>
2024-08-28 12:31:10 +0200

    remove metric serialization test

--- tests/unit/callbacks/metrics_configuration_callback/test_metrics_configuration_callback.py
+++ tests/unit/callbacks/metrics_configuration_callback/test_metrics_configuration_callback.py
@@ -3,13 +3,10 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from collections import OrderedDict
-from itertools import chain
 from pathlib import Path
 
 import lightning.pytorch as pl
 import pytest
-import torch
 from omegaconf import DictConfig, OmegaConf
 from torchvision.transforms.v2 import Resize
 
@@ -103,61 +100,3 @@ def test_metric_collection_configuration_callback(config_from_yaml: str, tmpdir:
         dummy_anomaly_module.pixel_metrics,
         AnomalibMetricCollection,
     ), f"{dummy_anomaly_module.pixel_metrics}"
-
-
-@pytest.mark.parametrize(
-    ("ori_config_from_yaml", "saved_config_from_yaml"),
-    [("data/config-good-02.yaml", "data/config-good-02-serialized.yaml")],
-)
-def test_metric_collection_configuration_deserialzation_callback(
-    ori_config_from_yaml: str,
-    saved_config_from_yaml: str,
-    tmpdir: str,
-) -> None:
-    """Test if metrics are properly instantiated during deserialzation."""
-    ori_config_from_yaml_res = OmegaConf.load(Path(__file__).parent / ori_config_from_yaml)
-    saved_config_from_yaml_res = OmegaConf.load(Path(__file__).parent / saved_config_from_yaml)
-    callback = _MetricsCallback(
-        task="segmentation",
-        image_metrics=ori_config_from_yaml_res.metrics.image,
-        pixel_metrics=ori_config_from_yaml_res.metrics.pixel,
-    )
-
-    dummy_anomaly_module = _DummyAnomalyModule()
-    trainer = pl.Trainer(
-        callbacks=[callback],
-        enable_checkpointing=False,
-        default_root_dir=tmpdir,
-    )
-
-    saved_image_state_dict = OrderedDict(
-        {
-            "image_metrics." + k: torch.tensor(1.0)
-            for k, v in saved_config_from_yaml_res.metrics.image.items()
-            if v["class_path"].startswith("anomalib.metrics")
-        },
-    )
-    saved_pixel_state_dict = OrderedDict(
-        {
-            "pixel_metrics." + k: torch.tensor(1.0)
-            for k, v in saved_config_from_yaml_res.metrics.pixel.items()
-            if v["class_path"].startswith("anomalib.metrics")
-        },
-    )
-
-    final_state_dict = OrderedDict(chain(saved_image_state_dict.items(), saved_pixel_state_dict.items()))
-
-    dummy_anomaly_module._load_metrics(final_state_dict)  # noqa: SLF001
-    callback.setup(trainer, dummy_anomaly_module)
-
-    assert isinstance(
-        dummy_anomaly_module.image_metrics,
-        AnomalibMetricCollection,
-    ), f"{dummy_anomaly_module.image_metrics}"
-    assert isinstance(
-        dummy_anomaly_module.pixel_metrics,
-        AnomalibMetricCollection,
-    ), f"{dummy_anomaly_module.pixel_metrics}"
-
-    for metric_name in ("AUROC", "F1Score"):
-        assert metric_name in dummy_anomaly_module.pixel_metrics
