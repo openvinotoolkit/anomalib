fb6fc0d52bdbe76da823271329adef11831679ac
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2024-10-09 10:40:44 +0200

    minor fixes
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/anomalib/callbacks/metrics.py
+++ src/anomalib/callbacks/metrics.py
@@ -75,7 +75,7 @@ class _MetricsCallback(Callback):
         pixel_metric_names: list[str] | dict[str, dict[str, Any]]
         if self.pixel_metric_names is None:
             pixel_metric_names = []
-        elif self.task in TaskType.CLASSIFICATION:
+        elif self.task == TaskType.CLASSIFICATION:
             pixel_metric_names = []
             logger.warning(
                 "Cannot perform pixel-level evaluation when task type is {self.task.value}. "
