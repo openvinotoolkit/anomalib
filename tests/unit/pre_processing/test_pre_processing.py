03196faa179af896b86e1c03024c80f5bfd00fce
Samet Akcay <samet.akcay@intel.com>
2024-10-29 17:31:19 +0000

    update preprocessor tests
    Signed-off-by: Samet Akcay <samet.akcay@intel.com>

--- tests/unit/pre_processing/test_pre_processing.py
+++ tests/unit/pre_processing/test_pre_processing.py
@@ -59,17 +59,18 @@ class TestPreProcessor:
     def test_callbacks(self, method_name: str) -> None:
         """Test the callbacks of the PreProcessor class."""
         pre_processor = PreProcessor(transform=self.common_transform)
+        trainer = MagicMock()
+        pl_module = MagicMock()
         method = getattr(pre_processor, method_name)
-        method(None, None, self.dummy_batch, 0)
+        method(trainer, pl_module, self.dummy_batch, 0)
         assert self.dummy_batch.image.shape == (1, 3, 224, 224)
         assert self.dummy_batch.gt_mask.shape == (1, 224, 224)
 
     def test_no_transform(self) -> None:
         """Test no transform."""
         pre_processor = PreProcessor()
-        processed_batch = pre_processor(self.dummy_batch)
-        assert processed_batch.image.shape == (1, 3, 256, 256)
-        assert processed_batch.gt_mask.shape == (1, 256, 256)
+        processed_batch = pre_processor(self.dummy_batch.image)
+        assert processed_batch.shape == (1, 3, 256, 256)
 
     @staticmethod
     def test_different_stage_transforms() -> None:
@@ -84,34 +85,23 @@ class TestPreProcessor:
             test_transform=test_transform,
         )
 
+        trainer = MagicMock()
+        pl_module = MagicMock()
+
         # Test train transform
         test_batch = ImageBatch(image=Image(torch.rand(3, 256, 256)), gt_mask=Mask(torch.zeros(256, 256)))
-        pre_processor.on_train_batch_start(None, None, test_batch, 0)
+        pre_processor.on_train_batch_start(trainer, pl_module, test_batch, 0)
         assert test_batch.image.shape == (1, 3, 224, 224)
 
         # Test validation transform
         test_batch = ImageBatch(image=Image(torch.rand(3, 256, 256)), gt_mask=Mask(torch.zeros(256, 256)))
-        pre_processor.on_validation_batch_start(None, None, test_batch, 0)
+        pre_processor.on_validation_batch_start(trainer, pl_module, test_batch, 0)
         assert test_batch.image.shape == (1, 3, 256, 256)
 
         # Test test transform
         test_batch = ImageBatch(image=Image(torch.rand(3, 256, 256)), gt_mask=Mask(torch.zeros(256, 256)))
-        pre_processor.on_test_batch_start(None, None, test_batch, 0)
+        pre_processor.on_test_batch_start(trainer, pl_module, test_batch, 0)
         assert test_batch.image.shape == (1, 3, 288, 288)
-
-    def test_setup_transforms_from_datamodule(self) -> None:
-        """Test setup method when transforms are obtained from datamodule."""
-        # Mock datamodule with transforms
-        datamodule = MagicMock()
-        datamodule.train_transform = self.common_transform
-        datamodule.eval_transform = self.common_transform
-
-        pre_processor = PreProcessor()
-        pre_processor.setup_transforms(datamodule=datamodule)
-
-        assert pre_processor.train_transform == self.common_transform
-        assert pre_processor.val_transform == self.common_transform
-        assert pre_processor.test_transform == self.common_transform
 
     def test_setup_transforms_from_dataloaders(self) -> None:
         """Test setup method when transforms are obtained from dataloaders."""
@@ -120,7 +110,7 @@ class TestPreProcessor:
         dataloader.dataset.transform = self.common_transform
 
         pre_processor = PreProcessor()
-        pre_processor.setup_transforms(dataloaders=[dataloader])
+        pre_processor.setup_dataloader_transforms(dataloaders=[dataloader])
 
         assert pre_processor.train_transform == self.common_transform
         assert pre_processor.val_transform == self.common_transform
@@ -141,7 +131,7 @@ class TestPreProcessor:
 
         # Initialize PreProcessor with a custom transform
         pre_processor = PreProcessor(transform=self.common_transform)
-        pre_processor.setup_transforms(datamodule=datamodule, dataloaders=[dataloader])
+        pre_processor.setup_datamodule_transforms(datamodule=datamodule)
 
         # Ensure PreProcessor's own transform is used
         assert pre_processor.train_transform == self.common_transform
