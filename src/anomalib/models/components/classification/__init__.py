bb5f24b6418db7e5292065ebe6ee163fd7a0884b
Samet Akcay <samet.akcay@intel.com>
2024-12-17 21:02:45 +0000

    Add model components docstrings
    Signed-off-by: Samet Akcay <samet.akcay@intel.com>

--- src/anomalib/models/components/classification/__init__.py
+++ src/anomalib/models/components/classification/__init__.py
@@ -1,4 +1,21 @@
-"""Classification modules."""
+"""Classification modules for anomaly detection.
+
+This module provides classification components used in anomaly detection models.
+
+Classes:
+    KDEClassifier: Kernel Density Estimation based classifier for anomaly
+        detection.
+    FeatureScalingMethod: Enum class defining feature scaling methods for
+        KDE classifier.
+
+Example:
+    >>> from anomalib.models.components.classification import KDEClassifier
+    >>> from anomalib.models.components.classification import FeatureScalingMethod
+    >>> # Create KDE classifier with min-max scaling
+    >>> classifier = KDEClassifier(
+    ...     scaling_method=FeatureScalingMethod.MIN_MAX
+    ... )
+"""
 
 # Copyright (C) 2022-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
