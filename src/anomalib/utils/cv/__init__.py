3a795c140ee483f6ad5ba0bc35887daec9d7373b
Samet Akcay <samet.akcay@intel.com>
2024-12-18 11:46:05 +0000

    Add utils
    Signed-off-by: Samet Akcay <samet.akcay@intel.com>

--- src/anomalib/utils/cv/__init__.py
+++ src/anomalib/utils/cv/__init__.py
@@ -1,4 +1,20 @@
-"""Anomalib computer vision utilities."""
+"""Computer vision utilities for anomaly detection.
+
+This module provides computer vision utilities used by the anomalib library for
+processing and analyzing images during anomaly detection.
+
+The utilities include:
+    - Connected components analysis for both CPU and GPU
+    - Image processing operations
+    - Computer vision helper functions
+
+Example:
+    >>> from anomalib.utils.cv import connected_components_cpu
+    >>> # Process image to get binary mask
+    >>> mask = get_binary_mask(image)
+    >>> # Find connected components
+    >>> labels = connected_components_cpu(mask)
+"""
 
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
