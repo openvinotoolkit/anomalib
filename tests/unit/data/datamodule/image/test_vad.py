a00b3db49506acb11ca84f342aa23903e94f18d5
Aimira Baitieva <63813435+abc-125@users.noreply.github.com>
2025-03-19 17:43:01 +0100

    Update tests/unit/data/datamodule/image/test_vad.py
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>

--- tests/unit/data/datamodule/image/test_vad.py
+++ tests/unit/data/datamodule/image/test_vad.py
@@ -1,6 +1,6 @@
 """Unit Tests - VAD Datamodule."""
 
-# Copyright (C) 2023-2025 Intel Corporation
+# Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 from pathlib import Path
