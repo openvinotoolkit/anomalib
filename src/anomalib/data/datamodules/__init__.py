bad9a6f1cf61ee3c32ff2eb521c888345edcae63
Samet Akcay <samet.akcay@intel.com>
2025-03-25 12:03:57 +0000

    Update __init__.py

--- src/anomalib/data/datamodules/__init__.py
+++ src/anomalib/data/datamodules/__init__.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .depth import Folder3D, MVTec3D
-from .image import BTech, Datumaro, Folder, Kolektor, MVTec, MVTecAD, VAD, Visa
+from .image import VAD, BTech, Datumaro, Folder, Kolektor, MVTec, MVTecAD, Visa
 from .video import Avenue, ShanghaiTech, UCSDped
 
 __all__ = [
